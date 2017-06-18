b863eb0427249373d3340ee27dcfcc3bf35eabb8
briandennis <briandennis321@gmail.com>
2017-06-17 20:47:25 -0700

    get editor working

--- components/codeImage.js
+++ components/codeImage.js
@@ -1,27 +1,16 @@
 import { EOL } from 'os'
 import React from 'react'
-import { NativeTypes } from 'react-dnd-html5-backend'
-import { DropTarget } from 'react-dnd'
 import domtoimage from 'dom-to-image'
-import highlight from 'highlight.js'
 import CodeMirror from 'react-codemirror'
 
+// hack to only call modes on browser
+if (typeof window !== 'undefined' && typeof window.navigator !== 'undefined') {
+  require('codemirror/mode/javascript/javascript');
+  require('codemirror/mode/xml/xml');
+  require('codemirror/mode/markdown/markdown');
+}
+
 const padding = '50px 50px'
-
-const DEFAULT_CODE = `
-const pluckDeep = key => obj => key.split('.').reduce((accum, key) => accum[key], obj)
-
-const compose = (...fns) => res => fns.reduce((accum, next) => next(accum), res)
-
-const unfold = (f, seed) => {
-  const go = (f, seed, acc) => {
-    const res = f(seed)
-    return res ? go(f, res[1], acc.concat([res[0]])) : acc
-  }
-  return go(f, seed, [])
-}`
-
-const MAX_LINES = 40
 
 class CodeImage extends React.Component {
   constructor (props) {
@@ -45,41 +34,21 @@ class CodeImage extends React.Component {
       })
   }
 
-  highlight () {
-    highlight.initHighlighting.called = false
-    highlight.initHighlighting()
-  }
-
-  componentDidMount () {
-    // highlight
-    this.highlight()
-
-    //highlight when content changes
-    this.editor.addEventListener('input', () => {
-      console.log('edit')
-      this.highlight()
-    })
+  updateCode (newCode) {
+    this.setState({ code: newCode })
   }
 
   render () {
-    let code = this.state.droppedContent || this.props.children || DEFAULT_CODE
-    const split = code.split(EOL)
-    if (split.length > MAX_LINES) code = [...split.slice(0, MAX_LINES - 1), '', '...'].join(EOL)
+    const options = { lineNumbers: false, mode: 'javascript' }
 
-    return this.props.connectDropTarget(
+    return (
       <div id='section'>
-        <link rel='stylesheet' href='https://highlightjs.org/static/demo/styles/default.css'/>
+        <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.26.0/codemirror.min.css'/>
         <div id='container' ref={(container) => { this.container = container }}>
-
-          <div contentEditable="true" ref={(editor) => { this.editor = editor}}>
-            <pre>
-              <code>
-                {this.props.initCode}
-              </code>
-            </pre>
-          </div>
+         <div id='anotherContainer'>
+           <CodeMirror value={this.state.code} onChange={this.updateCode} options={options} />
+         </div>
         </div>
-        <button onClick={this.highlight}/>
         <style jsx>{`
           #section {
             width: 100%;
@@ -92,10 +61,12 @@ class CodeImage extends React.Component {
             padding: ${padding};
            
           }
-          pre {
+          #anotherContainer {
             background: white;
-            padding: 10px;
-            box-shadow: 10px 10px 12px -5px rgba(0,0,0,0.17);
+            min-width: 700px;
+            min-height: 400px;
+            margin: 0px;
+            padding: 15px;
           }
         `}</style>
       </div>
@@ -103,21 +74,4 @@ class CodeImage extends React.Component {
   }
 }
 
-const drop = (props, monitor, component) => {
-  const bundle = monitor.getItem()
-  const file = bundle.files[0]
-  const reader = new FileReader()
-  reader.onload = function(event) {
-    component.setState({
-      droppedContent: event.target.result
-    })
-  };
-  reader.readAsText(file, "UTF-8");
-}
-
-export default DropTarget(NativeTypes.FILE, { drop }, (connect, monitor) => ({
-  connectDropTarget: connect.dropTarget(),
-  isOver: monitor.isOver(),
-  canDrop: monitor.canDrop(),
-  item: monitor.getItem()
-}))(CodeImage)
+export default CodeImage
--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,19 @@ import Axios from 'axios'
 import CodeImage from '../components/codeImage'
 import api from '../lib/api'
 
+const DEFAULT_CODE = `const pluckDeep = key => obj => key.split('.').reduce((accum, key) => accum[key], obj)
+
+const compose = (...fns) => res => fns.reduce((accum, next) => next(accum), res)
+
+const unfold = (f, seed) => {
+  const go = (f, seed, acc) => {
+    const res = f(seed)
+    return res ? go(f, res[1], acc.concat([res[0]])) : acc
+  }
+  return go(f, seed, [])
+}
+  `
+
 class Home extends React.Component {
   /* pathname, asPath, err, req, res */
   static async getInitialProps ({ asPath }) {
@@ -17,6 +30,7 @@ class Home extends React.Component {
       return {}
     }
   }
+
   render () {
     return (
       <div>
@@ -31,7 +45,7 @@ class Home extends React.Component {
         </style>
         <h1>Welcome to Code Image</h1>
         <CodeImage>
-          {this.props.content}
+          {DEFAULT_CODE}
         </CodeImage>
       </div>
     )
