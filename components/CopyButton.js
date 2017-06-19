3ddf969fb8a92c43dd36a2f6ddb4a6a513a350fc
briandennis <briandennis321@gmail.com>
2017-06-18 22:05:13 -0700

    add basic render function to copy button

--- components/CopyButton.js
+++ components/CopyButton.js
@@ -1,10 +1,20 @@
 import React from 'react'
 
+import Button from './Button'
+
 // constants
+const BUTTON_COLOR = '#84ACFC'
+
 const STATUS = {
   IDLE: 'IDLE',
   LOADING: 'LOADING',
   DEBOUNCED: 'DEBOUNCED'
+}
+
+const textMap = {
+  [STATUS.IDLE]: 'Copy Imgur Link',
+  [STATUS.LOADING]: 'Loading...',
+  [STATUS.DEBOUNCED]: 'Coppied'
 }
 
 class CopyButton extends React.Component {
@@ -15,4 +25,18 @@ class CopyButton extends React.Component {
       status: STATUS.IDLE
     }
   }
+
+  handleClick () {
+    switch (this.state.status)
+  }
+
+  render () {
+    return (
+      <div>
+        <Button onClick={() => {}} title={textMap[this.state.status]} bg={BUTTON_COLOR}/>
+      </div>
+    )
+  }
 }
+
+export default CopyButton
