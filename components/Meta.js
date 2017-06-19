51b0087a3bf7293528d1c42c5d37d78aef1b99b1
Mike Fix <mrfix84@gmail.com>
2017-06-18 18:34:57 -0700

    Link CDN from constants

--- components/Meta.js
+++ components/Meta.js
@@ -1,10 +1,15 @@
 import Head from 'next/head'
+import { THEMES } from '../lib/constants'
 
 export default () => (
   <div className="meta">
     <Head>
       <link rel="stylesheet" href='//cdnjs.cloudflare.com/ajax/libs/codemirror/5.26.0/codemirror.min.css' />
-      <link rel="stylesheet" href='//cdnjs.cloudflare.com/ajax/libs/codemirror/5.26.0/theme/dracula.min.css'/>
+      {
+        THEMES.map(theme => (
+          <link rel="stylesheet" href={`//cdnjs.cloudflare.com/ajax/libs/codemirror/5.26.0/theme/${theme.id}.min.css`}/>
+        ))
+      }
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
       <link rel="shortcut icon" href="/static/favicon.ico" />
