c5074e22f257f017465b6917e1f58ad1b519e9e7
Jake Dexheimer <jakedex@gmail.com>
2017-06-19 18:42:41 -0700

    Reactify themes SVG attributes

--- components/svg/Themes.js
+++ components/svg/Themes.js
@@ -5,14 +5,14 @@ export const Hyper = () => (
     <defs>
       <rect id="a" width="81" height="81" rx="3"/>
     </defs>
-    <g fill="none" fill-rule="evenodd">
+    <g fill="none" fillRule="evenodd">
       <mask id="b" fill="white">
         <use xlinkHref="#a"/>
       </mask>
       <use fill="#616161" xlinkHref="#a"/>
       <g transform="translate(17 33)" mask="url(#b)">
-        <path fill="#000000" stroke="#393939" stroke-width="2" d="M65.0458013,49.1077029 C66.0458013,49.1077029 0.174089069,49.1077029 0.174089069,49.1077029 L0.174089069,5.16868499 C0.174089069,2.41055979 2.40986586,0.174657534 5.17268563,0.174657534 L65.0458013,0.174657534 L65.0458013,49.1077029 Z"/>
-        <g fill-rule="nonzero" stroke-width=".5" transform="translate(18.96 14.27)">
+        <path fill="#000000" stroke="#393939" strokeWidth="2" d="M65.0458013,49.1077029 C66.0458013,49.1077029 0.174089069,49.1077029 0.174089069,49.1077029 L0.174089069,5.16868499 C0.174089069,2.41055979 2.40986586,0.174657534 5.17268563,0.174657534 L65.0458013,0.174657534 L65.0458013,49.1077029 Z"/>
+        <g fillRule="nonzero" strokeWidth=".5" transform="translate(18.96 14.27)">
           <ellipse cx="7.045" cy="7.048" fill="#FF5E55" stroke="#E0443E" rx="7.045" ry="7.048"/>
           <ellipse cx="30.526" cy="7.048" fill="#FFC02C" stroke="#DEA123" rx="7.045" ry="7.048"/>
         </g>
@@ -26,14 +26,14 @@ export const BW = () => (
     <defs>
       <rect id="a" width="81" height="81" rx="3"/>
     </defs>
-    <g fill="none" fill-rule="evenodd">
+    <g fill="none" fillRule="evenodd">
       <mask id="b" fill="white">
         <use xlinkHref="#a"/>
       </mask>
       <use fill="#616161" xlinkHref="#a"/>
       <g transform="translate(17 33)" mask="url(#b)">
-        <path fill="#000000" stroke="#FFFFFF" stroke-width="2" d="M65.0458013,49.1077029 C66.0458013,49.1077029 0.174089069,49.1077029 0.174089069,49.1077029 L0.174089069,5.16868499 C0.174089069,2.41055979 2.40986586,0.174657534 5.17268563,0.174657534 L65.0458013,0.174657534 L65.0458013,49.1077029 Z"/>
-        <g fill-rule="nonzero" stroke="#878787" transform="translate(18.96 14.27)">
+        <path fill="#000000" stroke="#FFFFFF" strokeWidth="2" d="M65.0458013,49.1077029 C66.0458013,49.1077029 0.174089069,49.1077029 0.174089069,49.1077029 L0.174089069,5.16868499 C0.174089069,2.41055979 2.40986586,0.174657534 5.17268563,0.174657534 L65.0458013,0.174657534 L65.0458013,49.1077029 Z"/>
+        <g fillRule="nonzero" stroke="#878787" transform="translate(18.96 14.27)">
           <ellipse cx="7.045" cy="7.048" rx="7.045" ry="7.048"/>
           <ellipse cx="30.526" cy="7.048" rx="7.045" ry="7.048"/>
         </g>
@@ -47,14 +47,14 @@ export const None = () => (
     <defs>
       <rect id="a" width="81" height="81" rx="3"/>
     </defs>
-    <g fill="none" fill-rule="evenodd">
+    <g fill="none" fillRule="evenodd">
       <mask id="b" fill="white">
         <use xlinkHref="#a"/>
       </mask>
       <use fill="#616161" xlinkHref="#a"/>
       <g transform="translate(16 32)" mask="url(#b)">
-        <path fill="#000000" fill-rule="nonzero" d="M66.0458013,46.1092762 C66.0458013,48.3193105 64.2622787,50.1077029 62.050805,50.1077029 L0.174089069,50.1077029 L0.174089069,6.16868499 C0.174089069,2.85738806 2.85846845,0.174657534 6.17268563,0.174657534 L66.0458013,0.174657534 L66.0458013,46.1092762 Z"/>
-        <g fill-rule="nonzero" stroke-width=".5" transform="translate(19.96 15.27)">
+        <path fill="#000000" fillRule="nonzero" d="M66.0458013,46.1092762 C66.0458013,48.3193105 64.2622787,50.1077029 62.050805,50.1077029 L0.174089069,50.1077029 L0.174089069,6.16868499 C0.174089069,2.85738806 2.85846845,0.174657534 6.17268563,0.174657534 L66.0458013,0.174657534 L66.0458013,46.1092762 Z"/>
+        <g fillRule="nonzero" strokeWidth=".5" transform="translate(19.96 15.27)">
           <ellipse cx="7.045" cy="7.048" fill="#FF5E55" stroke="#E0443E" rx="7.045" ry="7.048"/>
           <ellipse cx="30.526" cy="7.048" fill="#FFC02C" stroke="#DEA123" rx="7.045" ry="7.048"/>
         </g>
