b60afeaa7eb458febeb5bae83cc56205508550ac
Jake Dexheimer <jakedex@gmail.com>
2017-07-24 09:41:05 -0500

    Update /about copy

--- pages/about.js
+++ pages/about.js
@@ -9,7 +9,7 @@ export default () => (
     <div className="about">
       <div className="mb4">
         <h2>What does this do?</h2>
-        <p>You know all of those code screenshots you see on Twitter? Although the code's usually impressive, we think there's room for improvement in the aesthetic department. Carbon is the easiest way to create beautiful images of your source code.  Now you can impress all of your followers with your newfound design prowess. 🎨</p>
+        <p>You know all of those code screenshots you see on Twitter? Although the code's usually impressive, we saw room for improvement in the aesthetic department. Carbon is the easiest way to create beautiful images of your source code.  So what are you waiting for? Go you can impress all of your followers with your newfound design prowess. 🎨</p>
       </div>
       <div className="mb4">
         <h2>How do I use it?</h2>
@@ -17,13 +17,13 @@ export default () => (
         <p className="mb1 mt2">There are a few different ways to import code into Carbon:</p>
         <ul className="mt0 mb3">
           <li>Drag a file into the editor</li>
-          <li>Append a GitHub Gist id to the url</li>
+          <li>Append a GitHub gist id to the url (<a href="#">example</a>)</li>
           <li>Paste your code directly</li>
         </ul>
         <h4 className="mb0">Customization</h4>
         <p className="mt2 mb3">Once you've got all of your code into Carbon, you can customize your image by changing the syntax theme, background color, window theme, or padding.</p>
         <h4 className="mb0">Export/Sharing</h4>
-        <p className="mt2">After you've customized your image, you can copy a link to the image, save it, or share it on Twitter.</p>
+        <p className="mt2">After you've customized your image you can copy an Imgur link to the image, or save it directly.</p>
       </div>
       <div className="mb4">
         <h2>I want to make this better.</h2>
