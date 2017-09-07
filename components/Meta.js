109edb660dd02666847d63fb9689016d059ca523
Jake Dexheimer <jakedex@gmail.com>
2017-09-07 10:44:57 -0500

    Add social meta tags

--- components/Meta.js
+++ components/Meta.js
@@ -6,15 +6,24 @@ import Typography from './style/Typography'
 export default () => (
   <div className="meta">
     <Head>
+      <meta charSet="utf-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="description" content="Carbon is the easiest way to create and share beautiful images of your source code." />
+      <meta name="application-name" content="Carbon" />
+      <meta name="twitter:title" content="Carbon" />
+      <meta name="twitter:description" content="Carbon is the easiest way to create and share beautiful images of your source code." />
+      <meta name="og:title" content="Carbon" />
+      <meta name="og:description" content="Carbon is the easiest way to create and share beautiful images of your source code." />
+      <meta name="og:image" content="/static/banner.png" />
+      <title>Carbon</title>
+      <link rel="shortcut icon" href="/static/favicon.ico" />
       <link rel="stylesheet" href='//cdnjs.cloudflare.com/ajax/libs/codemirror/5.26.0/codemirror.min.css' />
       {
         THEMES.map((theme, i) => (
           <link key={i} rel="stylesheet" href={`//cdnjs.cloudflare.com/ajax/libs/codemirror/5.26.0/theme/${theme.id}.min.css`}/>
         ))
       }
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta charSet="utf-8" />
-      <link rel="shortcut icon" href="/static/favicon.ico" />
       <link rel="stylesheet" href="//cdn.jsdelivr.net/font-hack/2.020/css/hack-extended.min.css" />
       <link rel="stylesheet" type="text/css" href="/static/react-spinner.css" />
     </Head>
