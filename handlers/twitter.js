2f0f7f80ef78ca636e8122ded709fcef89ac16e0
Michael Fix <mfix@wisc.edu>
2017-09-14 23:01:13 -0700

    Update twitter.js

--- handlers/twitter.js
+++ handlers/twitter.js
@@ -1,6 +1,5 @@
 const Twitter = require('twitter')
 
-console.log(process.env);
 const client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
