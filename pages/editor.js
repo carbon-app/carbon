f71506ffbda16435a6946c5cffa8d9563e700502
briandennis <briandennis321@gmail.com>
2017-09-22 19:21:00 -0500

    move debounce to api

--- pages/editor.js
+++ pages/editor.js
@@ -3,8 +3,6 @@ import React from 'react'
 import HTML5Backend from 'react-dnd-html5-backend'
 import { DragDropContext } from 'react-dnd'
 import domtoimage from 'dom-to-image'
-import debounce from 'lodash.debounce'
-import ms from 'ms'
 
 // Ours
 import Page from '../components/Page'
@@ -113,7 +111,7 @@ class Editor extends React.Component {
               <div className="buttons">
                 <Button
                   className="tweetButton"
-                  onClick={debounce(this.upload, ms('5s'), {leading: true, trailing: false})}
+                  onClick={this.upload}
                   title={this.state.uploading ? 'Loading...' : 'Tweet Image'}
                   color="#57b5f9"
                   style={{ marginRight: '8px' }}
