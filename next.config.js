c828d7dd1eaad1a64c25879486bf2d909aa23243
Jake Dexheimer <jakedex@gmail.com>
2017-10-03 03:21:24 -0700

    Fix compiling time

--- next.config.js
+++ next.config.js
@@ -2,10 +2,11 @@ const Uglify = require('uglifyjs-webpack-plugin')
 
 module.exports = {
   // TODO remove `next.configs.js` when this closes: https://github.com/zeit/next.js/issues/1195
-  webpack: function(c) {
-    c.plugins = c.plugins.filter(plugin => plugin.constructor.name !== 'UglifyJsPlugin')
-
-    c.plugins.push(new Uglify())
+  webpack: function(c, { dev }) {
+    if (!dev) {
+      c.plugins = c.plugins.filter(plugin => plugin.constructor.name !== 'UglifyJsPlugin')
+      c.plugins.push(new Uglify())
+    }
 
     return c
   }
