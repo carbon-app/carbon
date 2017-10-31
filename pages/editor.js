497188aa1a0e39f2af0b8cc5b076904f5a57c074
Mike Fix <mrfix84@gmail.com>
2017-10-30 20:55:04 -0700

    > -> >= for query length

--- pages/editor.js
+++ pages/editor.js
@@ -31,7 +31,7 @@ import { getState, saveState } from '../lib/util'
 
 const removeQueryString = str => {
   const qI = str.indexOf('?')
-  return qI > 0 ? str.substr(0, qI) : str
+  return qI >= 0 ? str.substr(0, qI) : str
 }
 
 class Editor extends React.Component {
