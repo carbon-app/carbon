4078735cdc24b7cb5c6a72d160f46ea62feed819
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-01-19 21:25:43 -0500

    add Cypress integration tests (#162)
    * chore: add Cypress background color tests
    
    * add localStorage spec
    
    * add project id for Cypress dashboard
    
    * remove project id from cypress.json

---
+++ cypress/integration/localStorage-spec.js
@@ -0,0 +1,41 @@
+/* eslint-env mocha */
+/* global cy */
+import {editorVisible} from '../support'
+
+// usually we can visit the page before each test
+// but these tests use the url, which means wasted page load
+// so instead visit the desired url in each test
+
+describe('localStorage', () => {
+  const themeDropdown = () =>
+    cy.get('#toolbar .dropdown-container').first()
+
+  const pickTheme = (name = 'Blackboard') =>
+    themeDropdown()
+      .click()
+      .contains(name)
+      .click()
+
+  it('is empty initially', () => {
+    cy.visit('/')
+    editorVisible()
+    cy.window().its('localStorage')
+      .should('have.length', 0)
+  })
+
+  it('saves on theme change', () => {
+    cy.visit('/')
+    editorVisible()
+    pickTheme('Blackboard')
+    themeDropdown().contains('Blackboard')
+
+    cy.window().its('localStorage.CARBON_STATE')
+      .then(JSON.parse)
+      .its('theme').should('equal', 'blackboard')
+
+    // visiting page again restores theme from localStorage
+    cy.visit('/')
+    themeDropdown().contains('Blackboard')
+    cy.url().should('contain', 't=blackboard')
+  })
+})
