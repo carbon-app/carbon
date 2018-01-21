75d45e140741e0c5c254d508de937554215c8e6d
Mike Fix <mrfix84@gmail.com>
2018-01-20 19:33:51 -0800

    set -e to deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # Don't deploy if commit contains WIP
+set -e
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   if [[ $(git log -1 --pretty=%B --no-merges) != *"WIP"* ]]; then
     NOW_URL=$(now -e NODE_ENV=production --public --no-clipboard --team=dawn --token "$NOW_TOKEN")
