ff03d1a034f81fa68ea5a2aaa3971fa85edad61a
Mike Fix <mrfix84@gmail.com>
2018-01-21 16:57:45 -0800

    Fix CI

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 # Don't deploy if commit contains WIP
 set -e
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
   if [[ $(git log -1 --pretty=%B --no-merges) != *"WIP"* ]]; then
     NOW_URL=$(now -e NODE_ENV=production --public --no-clipboard --team=dawn --token "$NOW_TOKEN")
 
     curl -X POST \
       -H 'Content-type: application/json' \
-      --data "{\"text\": \":small_red_triangle: New Carbon PR Deployment for <https://github.com/dawnlabs/carbon/pulls/$TRAVIS_PULL_REQUEST|PR #$TRAVIS_PULL_REQUEST>\n:rocket: $NOW_URL\"}" \
+      --data "{\"text\": \":small_red_triangle: New Carbon PR Deployment for <https://github.com/dawnlabs/carbon/pulls/$TRAVIS_PULL_REQUEST|PR #$TRAVIS_PULL_REQUEST>\\n:rocket: $NOW_URL\"}" \
       "$DAWN_SLACK_WEBHOOK"
   fi
 fi
