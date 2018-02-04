d5f786bad9b15e9c48c8be2c0b61a8c9d70e0f07
Mike Fix <mrfix84@gmail.com>
2018-02-03 18:22:49 -0800

    Only deploy if NOW_TOKEN is set

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,12 +2,14 @@
 # Don't deploy if commit contains WIP
 set -e
 if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
-  if [[ $(git log -1 --pretty=%B --no-merges) != *"WIP"* ]]; then
-    NOW_URL=$(now -e NODE_ENV=production --public --no-clipboard --team=dawn --token="$NOW_TOKEN")
+  if [[ -z ${NOW_TOKEN+x} ]]; then
+    if [[ $(git log -1 --pretty=%B --no-merges) != *"WIP"* ]]; then
+      NOW_URL=$(now -e NODE_ENV=production --public --no-clipboard --team=dawn --token="$NOW_TOKEN")
 
-    curl -X POST \
+      curl -X POST \
       -H 'Content-type: application/json' \
       --data "{\"text\": \":small_red_triangle: New Carbon PR Deployment for <https://github.com/dawnlabs/carbon/pulls/$TRAVIS_PULL_REQUEST|PR #$TRAVIS_PULL_REQUEST>\\n:rocket: $NOW_URL\"}" \
       "$DAWN_SLACK_WEBHOOK"
+    fi
   fi
 fi
