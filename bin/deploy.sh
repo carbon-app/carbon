1951402d8a763663b430324096f6ce5609929a30
Mike Fix <mrfix84@gmail.com>
2018-02-04 13:06:40 -0800

    Make CLI not fail if NOW_TOKEN is invalid"

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,12 +4,12 @@ set -e
 if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
   if [[ -z ${NOW_TOKEN+x} ]]; then
     if [[ $(git log -1 --pretty=%B --no-merges) != *"WIP"* ]]; then
-      NOW_URL=$(now -e NODE_ENV=production --public --no-clipboard --team=dawn --token="$NOW_TOKEN")
+      NOW_URL=$(now -e NODE_ENV=production --public --no-clipboard --team=dawn --token="$NOW_TOKEN") || exit 0
 
       curl -X POST \
-      -H 'Content-type: application/json' \
-      --data "{\"text\": \":small_red_triangle: New Carbon PR Deployment for <https://github.com/dawnlabs/carbon/pulls/$TRAVIS_PULL_REQUEST|PR #$TRAVIS_PULL_REQUEST>\\n:rocket: $NOW_URL\"}" \
-      "$DAWN_SLACK_WEBHOOK"
+        -H 'Content-type: application/json' \
+        --data "{\"text\": \":small_red_triangle: New Carbon PR Deployment for <https://github.com/dawnlabs/carbon/pulls/$TRAVIS_PULL_REQUEST|PR #$TRAVIS_PULL_REQUEST>\\n:rocket: $NOW_URL\"}" \
+        "$DAWN_SLACK_WEBHOOK"
     fi
   fi
 fi
