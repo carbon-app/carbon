b44ecff6eae617ec61fe68cc67e973d1575d9b51
Mike Fix <mrfix84@gmail.com>
2018-02-03 16:52:13 -0800

    XSS

--- pages/editor.js
+++ pages/editor.js
@@ -33,11 +33,8 @@ import { getState, saveState } from '../lib/util'
 const removeQueryString = str => {
   const qI = str.indexOf('?')
   return (qI >= 0 ? str.substr(0, qI) : str)
-    .replace(/&/g, '&amp;')
     .replace(/</g, '&lt;')
     .replace(/>/g, '&gt;')
-    .replace(/"/g, '&quot;')
-    .replace(/'/g, '&#x27;')
     .replace(/\//g, '&#x2F;')
 }
 
