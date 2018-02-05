a567f3c50d1b4b8e95a41c057e70b5edf444592d
Arjun Komath <arjunkomath@gmail.com>
2018-02-05 23:12:54 +0530

    Support for manual resize

--- components/Carbon.js
+++ components/Carbon.js
@@ -8,6 +8,7 @@ import debounce from 'lodash.debounce'
 import ms from 'ms'
 import WindowControls from '../components/WindowControls'
 import CodeMirror from '../lib/react-codemirror'
+import { Resizable, ResizableBox } from 'react-resizable'
 import {
   COLORS,
   DEFAULT_LANGUAGE,
@@ -17,7 +18,7 @@ import {
 } from '../lib/constants'
 
 const DEFAULT_SETTINGS = {
-  paddingVertical: '50px',
+  paddingVertical: '25px',
   paddingHorizontal: '50px',
   marginVertical: '45px',
   marginHorizontal: '45px',
@@ -38,7 +39,11 @@ class Carbon extends React.Component {
 
     this.state = {
       loading: true,
-      language: props.config.language
+      language: props.config.language,
+      windowSize: {
+        width: 890,
+        height: 368
+      }
     }
 
     this.handleLanguageChange = this.handleLanguageChange.bind(this)
@@ -66,6 +71,16 @@ class Carbon extends React.Component {
   codeUpdated(newCode) {
     this.handleLanguageChange(newCode)
     this.props.updateCode(newCode)
+  }
+
+  onResize = (event, { element, size }) => {
+    this.setState({
+      windowSize: {
+        width: size.width,
+        height: size.height
+      }
+    })
+    this.props.onAspectRatioChange(size.width / size.height)
   }
 
   handleLanguageChange = debounce(
@@ -116,129 +131,145 @@ class Carbon extends React.Component {
         </style>
       </div>
     )
+
+    const draggableOpts = {
+      width: this.state.windowSize.width,
+      height: this.state.windowSize.height,
+      onResize: this.onResize,
+      // If you change this, be sure to update your css
+      handleSize: [10, 10],
+      lockAspectRatio: false,
+      axis: 'both',
+      minConstraints: [790, 200],
+      maxConstraints: [1280, 720]
+    }
+
     if (this.state.loading === false) {
       content = (
-        <div id="container">
-          {config.windowControls ? <WindowControls theme={config.windowTheme} /> : null}
-          <CodeMirror
-            className={`CodeMirror__container window-theme__${config.windowTheme}`}
-            onBeforeChange={(editor, meta, code) => this.codeUpdated(code)}
-            value={this.props.children}
-            options={options}
-          />
-          <div id="container-bg">
-            <div className="white eliminateOnRender" />
-            <div className="alpha eliminateOnRender" />
-            <div className="bg" />
-          </div>
-          <style jsx>{`
-            #container {
-              position: relative;
-              min-width: ${config.widthAdjustment ? '90px' : '680px'};
-              max-width: 1024px; /* The Fallback */
-              max-width: 92vw;
-              padding: ${config.paddingVertical} ${config.paddingHorizontal};
-            }
+        <ResizableBox {...draggableOpts}>
+          <div id="container">
+            {config.windowControls ? <WindowControls theme={config.windowTheme} /> : null}
+            <CodeMirror
+              className={`CodeMirror__container window-theme__${config.windowTheme}`}
+              onBeforeChange={(editor, meta, code) => this.codeUpdated(code)}
+              value={this.props.children}
+              options={options}
+            />
+            <div id="container-bg">
+              <div className="white eliminateOnRender" />
+              <div className="alpha eliminateOnRender" />
+              <div className="bg" />
+            </div>
+            <style jsx>{`
+              #container {
+                position: relative;
+                min-width: ${config.widthAdjustment ? '90px' : '680px'};
+                max-width: 1024px; /* The Fallback */
+                max-width: 92vw;
+                padding: ${config.paddingVertical} ${config.paddingHorizontal};
+                height: ${this.state.windowSize.height}px;
+              }
 
-            #container #container-bg {
-              position: absolute;
-              top: 0px;
-              right: 0px;
-              bottom: 0px;
-              left: 0px;
-            }
+              #container #container-bg {
+                position: absolute;
+                top: 0px;
+                right: 0px;
+                bottom: 0px;
+                left: 0px;
+              }
 
-            #container .white {
-              background: #fff;
-              position: absolute;
-              top: 0px;
-              right: 0px;
-              bottom: 0px;
-              left: 0px;
-            }
+              #container .white {
+                background: #fff;
+                position: absolute;
+                top: 0px;
+                right: 0px;
+                bottom: 0px;
+                left: 0px;
+              }
 
-            #container .bg {
-              ${this.props.config.backgroundMode === 'image'
-                ? `background: url(${backgroundImage});
+              #container .bg {
+                ${this.props.config.backgroundMode === 'image'
+                  ? `background: url(${backgroundImage});
                    background-size: cover;
                    background-repeat: no-repeat;`
-                : `background: ${this.props.config.backgroundColor || config.backgroundColor};
+                  : `background: ${this.props.config.backgroundColor || config.backgroundColor};
                    background-size: auto;
                    background-repeat: repeat;`} position: absolute;
-              top: 0px;
-              right: 0px;
-              bottom: 0px;
-              left: 0px;
-            }
+                top: 0px;
+                right: 0px;
+                bottom: 0px;
+                left: 0px;
+              }
 
-            #container .alpha {
-              position: absolute;
-              top: 0px;
-              right: 0px;
-              bottom: 0px;
-              left: 0px;
-              background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAMUlEQVQ4T2NkYGAQYcAP3uCTZhw1gGGYhAGBZIA/nYDCgBDAm9BGDWAAJyRCgLaBCAAgXwixzAS0pgAAAABJRU5ErkJggg==)
-                left center;
-            }
+              #container .alpha {
+                position: absolute;
+                top: 0px;
+                right: 0px;
+                bottom: 0px;
+                left: 0px;
+                background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAMUlEQVQ4T2NkYGAQYcAP3uCTZhw1gGGYhAGBZIA/nYDCgBDAm9BGDWAAJyRCgLaBCAAgXwixzAS0pgAAAABJRU5ErkJggg==)
+                  left center;
+              }
 
-            #container :global(.cm-s-dracula .CodeMirror-cursor) {
-              border-left: solid 2px #159588;
-            }
+              #container :global(.cm-s-dracula .CodeMirror-cursor) {
+                border-left: solid 2px #159588;
+              }
 
-            #container :global(.cm-s-solarized) {
-              box-shadow: none;
-            }
+              #container :global(.cm-s-solarized) {
+                box-shadow: none;
+              }
 
-            #container :global(.cm-s-solarized.cm-s-light) {
-              text-shadow: #eee8d5 0 1px;
-            }
+              #container :global(.cm-s-solarized.cm-s-light) {
+                text-shadow: #eee8d5 0 1px;
+              }
 
-            #container :global(.CodeMirror-gutters) {
-              background-color: unset;
-              border-right: none;
-            }
+              #container :global(.CodeMirror-gutters) {
+                background-color: unset;
+                border-right: none;
+              }
 
-            #container :global(.CodeMirror__container) {
-              min-width: inherit;
-              position: relative;
-              z-index: 1;
-              border-radius: 5px;
-              ${config.dropShadow
-                ? `box-shadow: 0 ${config.dropShadowOffsetY} ${
-                    config.dropShadowBlurRadius
-                  } rgba(0, 0, 0, 0.55)`
-                : ''};
-            }
+              #container :global(.CodeMirror__container) {
+                min-width: inherit;
+                position: relative;
+                z-index: 1;
+                border-radius: 5px;
+                ${config.dropShadow
+                  ? `box-shadow: 0 ${config.dropShadowOffsetY} ${
+                      config.dropShadowBlurRadius
+                    } rgba(0, 0, 0, 0.55)`
+                  : ''};
+              }
 
-            #container :global(.CodeMirror__container .CodeMirror) {
-              height: auto;
-              min-width: inherit;
-              padding: 18px 18px;
-              ${config.lineNumbers ? 'padding-left: 12px;' : ''} border-radius: 5px;
-              font-family: ${config.fontFamily}, monospace !important;
-              font-size: ${config.fontSize};
-              font-variant-ligatures: contextual;
-              font-feature-settings: 'calt' 1;
-              user-select: none;
-            }
+              #container :global(.CodeMirror__container .CodeMirror) {
+                height: auto;
+                min-width: inherit;
+                padding: 18px 18px;
+                ${config.lineNumbers ? 'padding-left: 12px;' : ''} border-radius: 5px;
+                font-family: ${config.fontFamily}, monospace !important;
+                font-size: ${config.fontSize};
+                font-variant-ligatures: contextual;
+                font-feature-settings: 'calt' 1;
+                user-select: none;
+              }
 
-            #container :global(.CodeMirror-scroll) {
-              overflow: hidden !important;
-            }
+              #container :global(.CodeMirror-scroll) {
+                overflow: hidden !important;
+              }
 
-            #container :global(.window-theme__sharp > .CodeMirror) {
-              border-radius: 0px;
-            }
+              #container :global(.window-theme__sharp > .CodeMirror) {
+                border-radius: 0px;
+              }
 
-            #container :global(.window-theme__bw > .CodeMirror) {
-              border: 2px solid ${COLORS.SECONDARY};
-            }
+              #container :global(.window-theme__bw > .CodeMirror) {
+                border: 2px solid ${COLORS.SECONDARY};
+              }
 
-            #container :global(.window-controls + .CodeMirror__container > .CodeMirror) {
-              padding-top: 48px;
-            }
-          `}</style>
-        </div>
+              #container :global(.window-controls + .CodeMirror__container > .CodeMirror) {
+                padding-top: 48px;
+              }
+            `}</style>
+          </div>
+        </ResizableBox>
       )
     }
 
