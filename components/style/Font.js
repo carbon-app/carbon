e7116478c9d17b70fc11518f9a03dc9a2bbd6fb4
Eric Adamski <er.adamski@gmail.com>
2018-04-10 07:51:38 -0400

    Add support for IBM Plex Mono (Italic) font

--- components/style/Font.js
+++ components/style/Font.js
@@ -51,6 +51,19 @@ export default () => (
 
       /* latin */
       @font-face {
+        font-family: 'IBM Plex Mono';
+        font-style: italic;
+        font-weight: 500;
+        src: local('IBM Plex Mono Medium Italic'), local('IBMPlexMono-MediumItalic'),
+          url(https://fonts.gstatic.com/s/ibmplexmono/v2/-F6sfjptAgt5VM-kVkqdyU8n1ioSJlR1gMoQPttozw.woff2)
+            format('woff2');
+        unicode-range: U + 0000-00ff, U + 0131, U + 0152-0153, U + 02bb-02bc, U + 02c6, U + 02da,
+          U + 02dc, U + 2000-206f, U + 2074, U + 20ac, U + 2122, U + 2191, U + 2193, U + 2212,
+          U + 2215, U + FEFF, U + FFFD;
+      }
+
+      /* latin */
+      @font-face {
         font-family: 'Anonymous Pro';
         font-style: normal;
         font-weight: 400;
