9a0c39727ee041da1ba88087ec6208689a3acfa1
Tim Deschryver <timdeschryver@outlook.com>
2018-04-23 18:15:23 +0200

    move gist language to its own test

--- cypress/integration/gist.js
+++ cypress/integration/gist.js
@@ -7,6 +7,12 @@ describe('Gist', () => {
     editorVisible()
 
     cy.contains('Y-Combinator implemented in JavaScript')
+  })
+
+  it('Should pull the language from the first Gist file', () => {
+    cy.visit('/3208813b324d82a9ebd197e4b1c3bae8')
+    editorVisible()
+
     cy.get('#downshift-input-Auto').should('have.value', 'JavaScript')
   })
 })
