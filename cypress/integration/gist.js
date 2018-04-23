5a952a6072ba2875798c7fc90df8083d3a4cbdd9
Tim Deschryver <timdeschryver@outlook.com>
2018-04-23 19:05:28 +0200

    Use gist language response - fix when no queryparams are given (#344)
    * fix when no queryparams are given
    e.g. http://carbon.now.sh/3208813b324d82a9ebd197e4b1c3bae8 would still not set the language property because the initialState is null
    
    * move gist language to its own test

--- cypress/integration/gist.js
+++ cypress/integration/gist.js
@@ -2,12 +2,17 @@
 /* global cy */
 import { editorVisible } from '../support'
 describe('Gist', () => {
-  const themeDropdown = () => cy.get('#toolbar .dropdown-container').first()
-
   it('Should pull text from the first Gist file', () => {
     cy.visit('/3208813b324d82a9ebd197e4b1c3bae8')
     editorVisible()
 
     cy.contains('Y-Combinator implemented in JavaScript')
+  })
+
+  it('Should pull the language from the first Gist file', () => {
+    cy.visit('/3208813b324d82a9ebd197e4b1c3bae8')
+    editorVisible()
+
+    cy.get('#downshift-input-Auto').should('have.value', 'JavaScript')
   })
 })
