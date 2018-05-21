0f81d51a5f63f5b0ada33da419c51965ae90058f
briandennis <briandennis321@gmail.com>
2018-05-21 15:55:09 -0500

    add alt text support for Twitter images

--- handlers/twitter.js
+++ handlers/twitter.js
@@ -1,23 +1,39 @@
-const Twitter = require('twitter')
+const Twitter = require('twit')
 const morph = require('morphmorph')
 
 const RATE_LIMIT_CODE = 420
+const MAX_ALT_TEXT_LENGTH = 420
 
 const client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token: process.env.TWITTER_ACCESS_TOKEN_KEY,
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 })
 
 const uploadImage = data => client.post('media/upload', { media_data: data })
-const uploadTweet = (media = {}) =>
-  client.post('statuses/update', {
-    status: `Carbon Copy #${media.media_id_string.slice(0, 8)}`,
-    media_ids: media.media_id_string
-  })
+const uploadMetadata = (altText, twitterRes = {}) => {
+  if (!altText)
+    return twitterRes
 
-const extractImageUrl = morph.get('entities.media.0.display_url')
+  const formattedAltText = altText.length > MAX_ALT_TEXT_LENGTH
+    ? `${altText.slice(0, MAX_ALT_TEXT_LENGTH - 3)}...`
+    : altText
+
+  return client
+    .post('media/metadata/create', {
+      media_id: extractMediaId(twitterRes),
+      alt_text: { text: formattedAltText }
+    })
+    .then(() => twitterRes)
+}
+const uploadTweet = (twitterRes = {}) => client.post('statuses/update', {
+    status: `Carbon Copy #${extractMediaId(twitterRes).slice(0, 8)}`,
+    media_ids: extractMediaId(twitterRes)
+})
+
+const extractMediaId = morph.get('data.media_id_string')
+const extractImageUrl = morph.get('data.entities.media.0.display_url')
 const extractErrorCode = morph.get('0.code')
 
 const respondSuccess = (res, url) => res.json({ url })
@@ -35,11 +51,12 @@ const respondFail = (res, err) => {
 }
 
 module.exports = (req, res) => {
-  if (!req.body.data) {
+  if (!req.body.imageData) {
     return res.status(400).send()
   }
 
-  return uploadImage(req.body.data)
+  return uploadImage(req.body.imageData)
+    .then(uploadMetadata.bind(null, req.body.altText))
     .then(uploadTweet)
     .then(extractImageUrl)
     .then(respondSuccess.bind(null, res))
