3f03463354eb65e120ca73b91ed06c578a3e4789
Mike Fix <mrfix84@gmail.com>
2018-06-17 10:17:44 -0700

    Fix spinner position

--- components/Carbon.js
+++ components/Carbon.js
@@ -1,6 +1,5 @@
 import React, { PureComponent } from 'react'
 import * as hljs from 'highlight.js'
-import Spinner from 'react-spinner'
 import ResizeObserver from 'resize-observer-polyfill'
 import debounce from 'lodash.debounce'
 import ms from 'ms'
@@ -29,7 +28,6 @@ class Carbon extends PureComponent {
     super(props)
 
     this.state = {
-      loading: true,
       language: props.config.language
     }
 
@@ -40,10 +38,6 @@ class Carbon extends PureComponent {
   }
 
   componentDidMount() {
-    this.setState({
-      loading: false
-    })
-
     this.setState(handleLanguageChange(this.props.children, this.props))
 
     const ro = new ResizeObserver(entries => {
@@ -93,160 +87,145 @@ class Carbon extends PureComponent {
       (this.props.config.backgroundImage && this.props.config.backgroundImageSelection) ||
       this.props.config.backgroundImage
 
-    // set content to spinner if loading, else editor
-    let content = (
-      <div>
-        <Spinner />
+    const content = (
+      <div id="container">
+        {config.windowControls ? (
+          <WindowControls
+            titleBar={this.props.titleBar}
+            theme={config.windowTheme}
+            handleTitleBarChange={this.handleTitleBarChange}
+          />
+        ) : null}
+        <CodeMirror
+          className={`CodeMirror__container window-theme__${config.windowTheme}`}
+          onBeforeChange={this.onBeforeChange}
+          value={this.props.children}
+          options={options}
+        />
+        {config.watermark && <Watermark />}
+        <div id="container-bg">
+          <div className="white eliminateOnRender" />
+          <div className="alpha eliminateOnRender" />
+          <div className="bg" />
+        </div>
         <style jsx>
           {`
-            div {
-              height: 352px;
+            #container {
+              position: relative;
+              min-width: ${config.widthAdjustment ? '90px' : '680px'};
+              max-width: 1024px; /* The Fallback */
+              max-width: 92vw;
+              padding: ${config.paddingVertical} ${config.paddingHorizontal};
+            }
+
+            #container :global(.watermark) {
+              fill-opacity: 0.3;
+              position: absolute;
+              z-index: 2;
+              bottom: calc(${config.paddingVertical} + 16px);
+              right: calc(${config.paddingHorizontal} + 16px);
+            }
+
+            #container #container-bg {
+              position: absolute;
+              top: 0px;
+              right: 0px;
+              bottom: 0px;
+              left: 0px;
+            }
+
+            #container .white {
+              background: #fff;
+              position: absolute;
+              top: 0px;
+              right: 0px;
+              bottom: 0px;
+              left: 0px;
+            }
+
+            #container .bg {
+              ${this.props.config.backgroundMode === 'image'
+                ? `background: url(${backgroundImage});
+                     background-size: cover;
+                     background-repeat: no-repeat;`
+                : `background: ${this.props.config.backgroundColor || config.backgroundColor};
+                     background-size: auto;
+                     background-repeat: repeat;`} position: absolute;
+              top: 0px;
+              right: 0px;
+              bottom: 0px;
+              left: 0px;
+            }
+
+            #container .alpha {
+              position: absolute;
+              top: 0px;
+              right: 0px;
+              bottom: 0px;
+              left: 0px;
+              background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAMUlEQVQ4T2NkYGAQYcAP3uCTZhw1gGGYhAGBZIA/nYDCgBDAm9BGDWAAJyRCgLaBCAAgXwixzAS0pgAAAABJRU5ErkJggg==);
+            }
+
+            #container :global(.cm-s-dracula .CodeMirror-cursor) {
+              border-left: solid 2px #159588;
+            }
+
+            #container :global(.cm-s-solarized) {
+              box-shadow: none;
+            }
+
+            #container :global(.cm-s-solarized.cm-s-light) {
+              text-shadow: #eee8d5 0 1px;
+            }
+
+            #container :global(.CodeMirror-gutters) {
+              background-color: unset;
+              border-right: none;
+            }
+
+            #container :global(.CodeMirror__container) {
+              min-width: inherit;
+              position: relative;
+              z-index: 1;
+              border-radius: 5px;
+              ${config.dropShadow
+                ? `box-shadow: 0 ${config.dropShadowOffsetY} ${
+                    config.dropShadowBlurRadius
+                  } rgba(0, 0, 0, 0.55)`
+                : ''};
+            }
+
+            #container :global(.CodeMirror__container .CodeMirror) {
+              height: auto;
+              min-width: inherit;
+              padding: 18px 18px;
+              ${config.lineNumbers ? 'padding-left: 12px;' : ''} border-radius: 5px;
+              font-family: ${config.fontFamily}, monospace !important;
+              font-size: ${config.fontSize};
+              font-variant-ligatures: contextual;
+              font-feature-settings: 'calt' 1;
+              user-select: none;
+            }
+
+            #container :global(.CodeMirror-scroll) {
+              overflow: hidden !important;
+            }
+
+            #container :global(.window-theme__sharp > .CodeMirror) {
+              border-radius: 0px;
+            }
+
+            #container :global(.window-theme__bw > .CodeMirror) {
+              border: 2px solid ${COLORS.SECONDARY};
+            }
+
+            #container :global(.window-controls + .CodeMirror__container > .CodeMirror) {
+              padding-top: 48px;
             }
           `}
         </style>
       </div>
     )
-    if (this.state.loading === false) {
-      content = (
-        <div id="container">
-          {config.windowControls ? (
-            <WindowControls
-              titleBar={this.props.titleBar}
-              theme={config.windowTheme}
-              handleTitleBarChange={this.handleTitleBarChange}
-            />
-          ) : null}
-          <CodeMirror
-            className={`CodeMirror__container window-theme__${config.windowTheme}`}
-            onBeforeChange={this.onBeforeChange}
-            value={this.props.children}
-            options={options}
-          />
-          {config.watermark && <Watermark />}
-          <div id="container-bg">
-            <div className="white eliminateOnRender" />
-            <div className="alpha eliminateOnRender" />
-            <div className="bg" />
-          </div>
-          <style jsx>
-            {`
-              #container {
-                position: relative;
-                min-width: ${config.widthAdjustment ? '90px' : '680px'};
-                max-width: 1024px; /* The Fallback */
-                max-width: 92vw;
-                padding: ${config.paddingVertical} ${config.paddingHorizontal};
-              }
-
-              #container :global(.watermark) {
-                fill-opacity: 0.3;
-                position: absolute;
-                z-index: 2;
-                bottom: calc(${config.paddingVertical} + 16px);
-                right: calc(${config.paddingHorizontal} + 16px);
-              }
-
-              #container #container-bg {
-                position: absolute;
-                top: 0px;
-                right: 0px;
-                bottom: 0px;
-                left: 0px;
-              }
-
-              #container .white {
-                background: #fff;
-                position: absolute;
-                top: 0px;
-                right: 0px;
-                bottom: 0px;
-                left: 0px;
-              }
-
-              #container .bg {
-                ${this.props.config.backgroundMode === 'image'
-                  ? `background: url(${backgroundImage});
-                   background-size: cover;
-                   background-repeat: no-repeat;`
-                  : `background: ${this.props.config.backgroundColor || config.backgroundColor};
-                   background-size: auto;
-                   background-repeat: repeat;`} position: absolute;
-                top: 0px;
-                right: 0px;
-                bottom: 0px;
-                left: 0px;
-              }
-
-              #container .alpha {
-                position: absolute;
-                top: 0px;
-                right: 0px;
-                bottom: 0px;
-                left: 0px;
-                background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAMUlEQVQ4T2NkYGAQYcAP3uCTZhw1gGGYhAGBZIA/nYDCgBDAm9BGDWAAJyRCgLaBCAAgXwixzAS0pgAAAABJRU5ErkJggg==);
-              }
-
-              #container :global(.cm-s-dracula .CodeMirror-cursor) {
-                border-left: solid 2px #159588;
-              }
-
-              #container :global(.cm-s-solarized) {
-                box-shadow: none;
-              }
-
-              #container :global(.cm-s-solarized.cm-s-light) {
-                text-shadow: #eee8d5 0 1px;
-              }
-
-              #container :global(.CodeMirror-gutters) {
-                background-color: unset;
-                border-right: none;
-              }
-
-              #container :global(.CodeMirror__container) {
-                min-width: inherit;
-                position: relative;
-                z-index: 1;
-                border-radius: 5px;
-                ${config.dropShadow
-                  ? `box-shadow: 0 ${config.dropShadowOffsetY} ${
-                      config.dropShadowBlurRadius
-                    } rgba(0, 0, 0, 0.55)`
-                  : ''};
-              }
-
-              #container :global(.CodeMirror__container .CodeMirror) {
-                height: auto;
-                min-width: inherit;
-                padding: 18px 18px;
-                ${config.lineNumbers ? 'padding-left: 12px;' : ''} border-radius: 5px;
-                font-family: ${config.fontFamily}, monospace !important;
-                font-size: ${config.fontSize};
-                font-variant-ligatures: contextual;
-                font-feature-settings: 'calt' 1;
-                user-select: none;
-              }
-
-              #container :global(.CodeMirror-scroll) {
-                overflow: hidden !important;
-              }
-
-              #container :global(.window-theme__sharp > .CodeMirror) {
-                border-radius: 0px;
-              }
-
-              #container :global(.window-theme__bw > .CodeMirror) {
-                border: 2px solid ${COLORS.SECONDARY};
-              }
-
-              #container :global(.window-controls + .CodeMirror__container > .CodeMirror) {
-                padding-top: 48px;
-              }
-            `}
-          </style>
-        </div>
-      )
-    }
 
     return (
       <div id="section">
