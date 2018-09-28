3d5c98ad54456fa314ccae61fe9b99c5fb00d1fb
Mike Fix <mrfix84@gmail.com>
2018-09-28 11:29:58 -0700

    stateful toast

--- components/Toast.js
+++ components/Toast.js
@@ -1,46 +1,70 @@
 import React from 'react'
 
-const Toast = () => (
-  <div className="toast mb4">
-    <div className="toast-content">
-      <p>Enjoy using Carbon? Want to work with the team that created it?</p>
-      <a href="https://dawnlabs.io/jobs">We&#39;re hiring!</a>
-      <a className="close-toast" href="#">
-        &times;
-      </a>
-    </div>
-    <style jsx>
-      {`
-        .toast {
-          width: 704px;
-          padding: 8px 16px;
-          background: #fff;
-          color: #000;
-          border-radius: 2px;
-          margin-top: calc(var(--x4) * -1);
-        }
+const key = 'CARBON_DAWN_TOAST'
 
-        .toast-content {
-          display: flex;
-          align-items: center;
-        }
+class Toast extends React.Component {
+  state = {
+    open: true
+  }
 
-        a {
-          text-decoration: underline;
-        }
+  close = () => this.setState({ open: false })
 
-        .close-toast {
-          text-decoration: none;
-          margin-left: auto;
-        }
+  componentDidMount() {
+    if (window.localStorage.getItem(key)) {
+      this.close()
+    } else {
+      window.localStorage.setItem(key, true)
+    }
+  }
 
-        p {
-          margin: 0;
-          margin-right: 12px;
-        }
-      `}
-    </style>
-  </div>
-)
+  render() {
+    if (!this.state.open) {
+      return null
+    }
+
+    return (
+      <div className="toast mb4">
+        <div className="toast-content">
+          <p>Enjoy using Carbon? Want to work with the team that created it?</p>
+          <a href="https://dawnlabs.io/jobs">We&#39;re hiring!</a>
+          <a className="close-toast" href="#" onClick={this.close}>
+            &times;
+          </a>
+        </div>
+        <style jsx>
+          {`
+            .toast {
+              width: 704px;
+              padding: 8px 16px;
+              background: #fff;
+              color: #000;
+              border-radius: 2px;
+              margin-top: calc(var(--x4) * -1);
+            }
+
+            .toast-content {
+              display: flex;
+              align-items: center;
+            }
+
+            a {
+              text-decoration: underline;
+            }
+
+            .close-toast {
+              text-decoration: none;
+              margin-left: auto;
+            }
+
+            p {
+              margin: 0;
+              margin-right: 12px;
+            }
+          `}
+        </style>
+      </div>
+    )
+  }
+}
 
 export default Toast
