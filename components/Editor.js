a174c26067874efc07adeaf458349b2ba5764e5f
Mike Fix <mrfix84@gmail.com>
2018-12-04 15:46:10 -0800

    fix another encoding bug...🤞
    - Closes #564

--- components/Editor.js
+++ components/Editor.js
@@ -144,7 +144,7 @@ class Editor extends React.Component {
 
     if (isPNG) {
       node.querySelectorAll('span[role="presentation"]').forEach(node => {
-        if (node.innerText && node.innerText.match(/%\d\S/)) {
+        if (node.innerText && node.innerText.match(/%\S\S/)) {
           map.set(node, node.innerText)
           node.innerText = encodeURIComponent(node.innerText)
         }
