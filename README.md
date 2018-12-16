3174a83adbbfbc2a4031df7a75a8d0fa028643e9
Rachel M. Carmena <rachelcarmena@users.noreply.github.com>
2018-12-16 20:48:53 +0100

    Caution about the use of images in a post (#607)
    * Caution about the use of images in a post
    
    * Update README.md
    
    * Update about.js
    
    * tweaks

--- README.md
+++ README.md
@@ -54,6 +54,10 @@ Once you've got all of your code into Carbon, you can customize your image by ch
 
 After you've customized your image you can either Tweet a link to the image, or save it directly.
 
+If you use the &apos;Tweet&apos; button, Carbon will automatically make your image accessible. However, if you want to manually tweet your carbon image, please check out [how to make your Twitter images accessible](https://help.twitter.com/en/using-twitter/picture-descriptions).
+
+If you include a Carbon image in a post, the source code will be invisible for assistive technology, it will not be possible to enlarge it or copy it, etc. Please, think about adding another element with the source code as text, like an [HTML Details Element](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/details) below the image.
+
 ## Community
 Check out these projects our awesome community has created:
 
--- pages/about.js
+++ pages/about.js
@@ -47,11 +47,23 @@ export default () => (
         </p>
         <p className="mt2 mb3">
           If you use the &apos;Tweet&apos; button, Carbon will automatically make your image
-          accessible. However, if you want to manually tweet your carbon image, please check out{' '}
+          accessible. However, if you want to manually tweet your carbon image, please check out (
           <a className="link" href="https://help.twitter.com/en/using-twitter/picture-descriptions">
-            this page
-          </a>{' '}
-          to found out how to make your Twitter images accessible.
+            how to make your Twitter images accessible
+          </a>
+          ).
+        </p>
+        <p className="mt2 mb3">
+          If you include a Carbon image in a post, the source code will be invisible to assistive
+          technology — it will not be possible to enlarge or copy it, etc. Please, think about
+          adding another element with the source code as text, like (
+          <a
+            className="link"
+            href="https://developer.mozilla.org/en-US/docs/Web/HTML/Element/details"
+          >
+            an HTML Details Element
+          </a>
+          ) below the image.
         </p>
       </div>
       <div>
