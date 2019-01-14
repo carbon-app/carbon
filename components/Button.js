cc593a79682897eff40cb7adac34547bbfd296c4
Mike Fix <mrfix84@gmail.com>
2019-01-14 09:13:46 -0800

    allow id's in <Button />

--- components/Button.js
+++ components/Button.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { COLORS } from '../lib/constants'
 
 const Button = ({
+  id,
   onClick = () => {},
   className = '',
   background = COLORS.BLACK,
@@ -21,7 +22,13 @@ const Button = ({
   padding = 0,
   margin = 0
 }) => (
-  <button onClick={onClick} className={className} disabled={disabled || notAllowed} style={style}>
+  <button
+    id={id}
+    onClick={onClick}
+    className={className}
+    disabled={disabled || notAllowed}
+    style={style}
+  >
     {children}
     <style jsx>
       {`
