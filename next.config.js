dcea10b16b7d324ca97df2e75d97313bafa1d7bc
raboid <sean@dawnlabs.io>
2019-01-23 17:33:17 -0500

    revert next.config.js

--- next.config.js
+++ next.config.js
@@ -1,32 +1,23 @@
 // const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
 // const withOffline = require('next-offline')
 
-const withBundleAnalyzer = require('@zeit/next-bundle-analyzer')
-
-module.exports = withBundleAnalyzer({
-  analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
-  analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
-  bundleAnalyzerConfig: {
-    server: {
-      analyzerMode: 'static',
-      reportFilename: '../bundles/server.html'
+module.exports = (/* phase  { defaultConfig } */) => {
+  const config = {
+    async exportPathMap() {
+      return {
+        '/about': { page: '/about' },
+        '/embed': { page: '/embed' },
+        '/index': { page: '/index' },
+        '/': { page: '/' }
+      }
     },
-    browser: {
-      analyzerMode: 'static',
-      reportFilename: '../bundles/client.html'
+    publicRuntimeConfig: {
+      API_URL:
+        process.env.NODE_ENV === 'production'
+          ? 'https://carbon-api.now.sh'
+          : 'http://localhost:4000'
     }
   }
 
-  // async exportPathMap() {
-  //   return {
-  //     '/about': { page: '/about' },
-  //     '/embed': { page: '/embed' },
-  //     '/index': { page: '/index' },
-  //     '/': { page: '/' }
-  //   }
-  // },
-  // publicRuntimeConfig: {
-  //   API_URL:
-  //     process.env.NODE_ENV === 'production' ? 'https://carbon-api.now.sh' : 'http://localhost:4000'
-  // },
-})
+  return config
+}
