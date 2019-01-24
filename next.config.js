5cecbc912f018915f382c45183533ebbd1918bb1
Mike Fix <mrfix84@gmail.com>
2019-01-24 12:27:28 -0800

    simplify next.config.js

--- next.config.js
+++ next.config.js
@@ -1,23 +1,8 @@
 // const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
 // const withOffline = require('next-offline')
 
-module.exports = (/* phase  { defaultConfig } */) => {
-  const config = {
-    async exportPathMap() {
-      return {
-        '/about': { page: '/about' },
-        '/embed': { page: '/embed' },
-        '/index': { page: '/index' },
-        '/': { page: '/' }
-      }
-    },
-    publicRuntimeConfig: {
-      API_URL:
-        process.env.NODE_ENV === 'production'
-          ? 'https://carbon-api.now.sh'
-          : 'http://localhost:4000'
-    }
+module.exports = {
+  publicRuntimeConfig: {
+    API_URL: process.env.API_URL || 'http://localhost:4000'
   }
-
-  return config
 }
