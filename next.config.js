75b2f05ef7c0ae9e5f666c0ea3150b7ee994c648
Mike Fix <mrfix84@gmail.com>
2019-02-18 15:13:50 -0800

    add offline support

--- next.config.js
+++ next.config.js
@@ -1,2 +1,24 @@
-// const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
-// const withOffline = require('next-offline')
+const withOffline = require('next-offline')
+
+module.exports = withOffline({
+  workboxOpts: {
+    swDest: 'static/service-worker.js',
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: 'networkFirst',
+        options: {
+          cacheName: 'https-calls',
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
+          },
+          cacheableResponse: {
+            statuses: [0, 200]
+          }
+        }
+      }
+    ]
+  }
+})
