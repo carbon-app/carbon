3d29034bff3c918e4d0837f850a80095e95baf3a
Mike Fix <mrfix84@gmail.com>
2019-03-13 16:37:24 -0700

    simplify next-offline config

--- next.config.js
+++ next.config.js
@@ -1,24 +1,3 @@
 const withOffline = require('next-offline')
 
-module.exports = withOffline({
-  workboxOpts: {
-    swDest: 'static/service-worker.js',
-    runtimeCaching: [
-      {
-        urlPattern: /^https?.*/,
-        handler: 'networkFirst',
-        options: {
-          cacheName: 'https-calls',
-          networkTimeoutSeconds: 15,
-          expiration: {
-            maxEntries: 150,
-            maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
-          },
-          cacheableResponse: {
-            statuses: [0, 200]
-          }
-        }
-      }
-    ]
-  }
-})
+module.exports = withOffline()
