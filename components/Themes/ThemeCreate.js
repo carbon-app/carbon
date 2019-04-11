d2bf3bb06b5c252e81d4d3ab4e30489dc22e4aa4
raboid <rabautse@gmail.com>
2019-04-11 17:57:24 -0400

    rework logic

--- components/Themes/ThemeCreate.js
+++ components/Themes/ThemeCreate.js
@@ -6,7 +6,7 @@ import ListSetting from '../ListSetting'
 import Popout from '../Popout'
 import ColorPicker from '../ColorPicker'
 import { HIGHLIGHT_KEYS, COLORS } from '../../lib/constants'
-import { capitalize } from '../../lib/util'
+import { capitalize, stringifyRGBA, generateId } from '../../lib/util'
 
 const colorPickerStyle = {
   backgroundColor: COLORS.BLACK,
@@ -47,143 +47,152 @@ const HighlightPicker = ({ title, onChange, color }) => (
   </div>
 )
 
-const ThemeCreate = ({
-  theme,
-  themes,
-  highlights,
-  name,
-  preset,
-  selected,
-  createTheme,
-  applyPreset,
-  updateName,
-  selectHighlight,
-  updateHighlight
-}) => (
-  <Popout pointerLeft="15px" style={{ display: 'flex' }}>
-    <div className="theme-settings">
-      <div className="field name-field">
-        <span>Name</span>
-        <Input
-          title="name"
-          name="name"
-          placeholder="Custom Theme"
-          value={name}
-          onChange={updateName}
-          maxLength="32"
-        />
-      </div>
-      <div className="theme-select">
-        <ListSetting
-          title="Preset"
-          items={themes}
-          selected={preset || theme}
-          onOpen={selectHighlight(null)}
-          onChange={applyPreset}
+const getCustomName = themes =>
+  `Custom Theme ${themes.filter(({ name }) => name.startsWith('Custom Theme')).length + 1}`
+
+const ThemeCreate = ({ theme, themes, highlights, create, updateHighlights }) => {
+  const [preset, updatePreset] = React.useState(theme.id)
+  const [highlight, selectHighlight] = React.useState()
+  const [name, updateName] = React.useState(getCustomName(themes))
+
+  return (
+    <Popout pointerLeft="15px" style={{ display: 'flex' }}>
+      <div className="theme-settings">
+        <div className="field name-field">
+          <span>Name</span>
+          <Input
+            title="name"
+            name="name"
+            placeholder="Custom Theme"
+            value={name}
+            onChange={({ target: { value } }) => updateName(value)}
+            maxLength="32"
+          />
+        </div>
+        <div className="theme-select">
+          <ListSetting
+            title="Preset"
+            items={themes}
+            selected={preset}
+            onOpen={() => selectHighlight(null)}
+            onChange={id => {
+              updatePreset(id)
+              updateHighlights(themes.find(t => t.id === id).highlights)
+            }}
+          >
+            {({ name }) => <span>{name}</span>}
+          </ListSetting>
+        </div>
+        <div className="theme-colors">
+          {HIGHLIGHT_KEYS.map(key => (
+            <div className="field" key={key}>
+              <Button
+                padding="4px 4px 4px 8px"
+                onClick={() => selectHighlight(key)}
+                background={highlight === key ? COLORS.HOVER : COLORS.BLACK}
+              >
+                <div className="row">
+                  <span>{capitalize(key)}</span>
+                  <span
+                    className="color-square"
+                    style={{
+                      backgroundColor: highlights[key],
+                      boxShadow: `inset 0px 0px 0px ${highlight === key ? 2 : 1}px ${
+                        COLORS.SECONDARY
+                      }`
+                    }}
+                  />
+                </div>
+              </Button>
+            </div>
+          ))}
+        </div>
+        <Button
+          center
+          disabled={!name}
+          className="create-button"
+          padding="8px 0"
+          background={COLORS.SECONDARY}
+          hoverBackground={COLORS.SECONDARY}
+          color={COLORS.BLACK}
+          onClick={() =>
+            create({
+              id: `theme:${generateId()}`,
+              name,
+              highlights,
+              custom: true
+            })
+          }
         >
-          {({ name }) => <span>{name}</span>}
-        </ListSetting>
+          Create +
+        </Button>
       </div>
-      <div className="theme-colors">
-        {HIGHLIGHT_KEYS.map(key => (
-          <div className="field" key={key}>
-            <Button
-              padding="4px 4px 4px 8px"
-              onClick={selectHighlight(key)}
-              background={selected === key ? COLORS.HOVER : COLORS.BLACK}
-            >
-              <div className="row">
-                <span>{capitalize(key)}</span>
-                <span
-                  className="color-square"
-                  style={{
-                    backgroundColor: highlights[key],
-                    boxShadow: `inset 0px 0px 0px ${selected === key ? 2 : 1}px ${COLORS.SECONDARY}`
-                  }}
-                />
-              </div>
-            </Button>
-          </div>
-        ))}
-      </div>
-      <Button
-        center
-        disabled={!name}
-        className="create-button"
-        padding="8px 0"
-        background={COLORS.SECONDARY}
-        hoverBackground={COLORS.SECONDARY}
-        color={COLORS.BLACK}
-        onClick={createTheme}
-      >
-        Create +
-      </Button>
-    </div>
-    {selected && (
-      <HighlightPicker
-        title={capitalize(selected)}
-        color={highlights[selected]}
-        onChange={updateHighlight}
-      />
-    )}
-    <style jsx>
-      {`
-        .field {
-          align-items: center;
-          border-bottom: solid 1px ${COLORS.SECONDARY};
-          display: flex;
-          height: 35px;
-          justify-content: space-between;
-          position: relative;
-        }
+      {highlight && (
+        <HighlightPicker
+          title={capitalize(highlight)}
+          color={highlights[highlight]}
+          onChange={({ rgb }) => updateHighlights({ [highlight]: stringifyRGBA(rgb) })}
+        />
+      )}
+      <style jsx>
+        {`
+          .field {
+            align-items: center;
+            border-bottom: solid 1px ${COLORS.SECONDARY};
+            display: flex;
+            height: 35px;
+            justify-content: space-between;
+            position: relative;
+          }
 
-        .field:nth-last-child(-n + 2) {
-          border-width: 2px;
-        }
+          .field:nth-last-child(-n + 2) {
+            border-width: 2px;
+          }
 
-        .row {
-          display: flex;
-          flex: 1;
-          align-items: center;
-          justify-content: space-between;
-        }
+          .row {
+            display: flex;
+            flex: 1;
+            align-items: center;
+            justify-content: space-between;
+          }
 
-        .name-field {
-          padding: 8px;
-        }
+          .name-field {
+            padding: 8px;
+          }
 
-        .theme-select {
-          width: 100%;
-        }
+          .theme-select {
+            width: 100%;
+          }
 
-        .theme-settings {
-          display: flex;
-          flex-direction: column;
-          width: 224px;
-        }
+          .theme-settings {
+            display: flex;
+            flex-direction: column;
+            width: 224px;
+          }
 
-        .theme-colors {
-          display: flex;
-          flex-wrap: wrap;
-          border-top: 2px solid ${COLORS.SECONDARY};
-          text-transform: capitalize;
-        }
+          .theme-colors {
+            display: flex;
+            flex-wrap: wrap;
+            border-top: 2px solid ${COLORS.SECONDARY};
+            text-transform: capitalize;
+          }
 
-        .theme-colors .field {
-          width: 50%;
-        }
+          .theme-colors .field {
+            width: 50%;
+          }
 
-        .theme-colors .field:nth-child(odd) {
-          border-right: 1px solid ${COLORS.SECONDARY};
-        }
+          .theme-colors .field:nth-child(odd) {
+            border-right: 1px solid ${COLORS.SECONDARY};
+          }
 
-        .color-square {
-          border-radius: 3px;
-          padding: 12px;
-        }
-      `}
-    </style>
-  </Popout>
-)
+          .color-square {
+            border-radius: 3px;
+            padding: 12px;
+          }
+        `}
+      </style>
+    </Popout>
+  )
+}
 
 export default ThemeCreate
