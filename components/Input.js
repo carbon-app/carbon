297c385fbba5ce3cf8ce7ddc43ad743906c7ea02
Mike Fix <mrfix84@gmail.com>
2019-04-14 18:06:38 -0700

    pass all props through to <input />

--- components/Input.js
+++ components/Input.js
@@ -2,64 +2,36 @@ import React from 'react'
 
 import { COLORS } from '../lib/constants'
 
-const Input = React.forwardRef(
-  (
-    {
-      accept,
-      color = COLORS.SECONDARY,
-      name,
-      onChange = () => {},
-      placeholder,
-      title,
-      type,
-      value,
-      align = 'right',
-      maxLength
-    },
-    ref
-  ) => {
-    return (
-      <React.Fragment>
-        <input
-          ref={ref}
-          type={type}
-          accept={accept}
-          title={title}
-          placeholder={placeholder}
-          value={value}
-          name={name}
-          onChange={onChange}
-          maxLength={maxLength}
-        />
-        <style jsx>
-          {`
-            input {
-              width: 100%;
-              font-size: 12px;
-              color: ${color};
-              background: transparent;
-              border: none;
-              outline: none;
-              text-align: ${align};
-              overflow: hidden;
-              white-space: nowrap;
-              text-overflow: ellipsis;
-            }
+const Input = React.forwardRef(({ color = COLORS.SECONDARY, align = 'right', ...props }, ref) => (
+  <React.Fragment>
+    <input ref={ref} {...props} />
+    <style jsx>
+      {`
+        input {
+          width: 100%;
+          font-size: 12px;
+          color: ${color};
+          background: transparent;
+          border: none;
+          outline: none;
+          text-align: ${align};
+          overflow: hidden;
+          white-space: nowrap;
+          text-overflow: ellipsis;
+        }
 
-            input::placeholder {
-              color: ${color};
-              opacity: 0.4;
-            }
+        input::placeholder {
+          color: ${color};
+          opacity: 0.4;
+        }
 
-            input[type='file'] {
-              cursor: pointer;
-              outline: none;
-            }
-          `}
-        </style>
-      </React.Fragment>
-    )
-  }
-)
+        input[type='file'] {
+          cursor: pointer;
+          outline: none;
+        }
+      `}
+    </style>
+  </React.Fragment>
+))
 
 export default Input
