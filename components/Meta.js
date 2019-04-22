4571857e6933377e6ae4573b5d5622187cb26838
Mike Fix <mrfix84@gmail.com>
2019-04-22 13:04:53 -0700

    make shades-of-purple local stylesheet

--- components/Meta.js
+++ components/Meta.js
@@ -5,7 +5,14 @@ import Reset from './style/Reset'
 import Font from './style/Font'
 import Typography from './style/Typography'
 
-const LOCAL_STYLESHEETS = ['one-light', 'one-dark', 'verminal', 'night-owl', 'nord']
+const LOCAL_STYLESHEETS = [
+  'one-light',
+  'one-dark',
+  'verminal',
+  'night-owl',
+  'nord',
+  'shades-of-purple'
+]
 const CDN_STYLESHEETS = THEMES.filter(t => LOCAL_STYLESHEETS.indexOf(t.id) < 0)
 
 export function Link({ href }) {
