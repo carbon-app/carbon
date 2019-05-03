28a455eecc945dd7a83468c8f350e4a8d55ea9b5
Mike Fix <mrfix84@gmail.com>
2019-05-03 10:37:54 -0700

    remove unused css

--- components/style/Typography.js
+++ components/style/Typography.js
@@ -16,7 +16,6 @@ export default () => (
         --h5: 0.75rem;
 
         --lh: calc(4 / 3);
-        --mx: 32em;
 
         --m1: calc(2 / 3 * 1em);
         --m2: calc(4 / 3 * 1em);
@@ -70,64 +69,6 @@ export default () => (
         font-size: var(--h5);
       }
 
-      .h0 {
-        font-size: var(--h0);
-      }
-      .h1 {
-        font-size: var(--h1);
-      }
-      .h2 {
-        font-size: var(--h2);
-      }
-      .h3 {
-        font-size: var(--h3);
-      }
-      .h4 {
-        font-size: var(--h4);
-      }
-      .h5 {
-        font-size: var(--h5);
-      }
-      .h6 {
-        font-size: var(--h5);
-      }
-
-      @media screen and (min-width: 40em) {
-        .xh0 {
-          font-size: var(--h0);
-        }
-        .xh1 {
-          font-size: var(--h1);
-        }
-        .xh2 {
-          font-size: var(--h2);
-        }
-        .xh3 {
-          font-size: var(--h3);
-        }
-        .xh4 {
-          font-size: var(--h4);
-        }
-        .xh5 {
-          font-size: var(--h5);
-        }
-        .xh6 {
-          font-size: var(--h5);
-        }
-      }
-
-      .lh1 {
-        line-height: 1;
-      }
-      /* h0, h1, h3 */
-      .lh2 {
-        line-height: calc(7 / 6 * 1em);
-      }
-      /* For body copy */
-      .lh3 {
-        line-height: calc(16 / 9 * 1em);
-      }
-
       .mt0 {
         margin-top: 0;
       }
@@ -169,13 +110,6 @@ export default () => (
       }
       .mb6 {
         margin-bottom: var(--x6);
-      }
-
-      .mx {
-        max-width: var(--mx);
-      }
-      .bold {
-        font-weight: bold;
       }
     `}
   </style>
