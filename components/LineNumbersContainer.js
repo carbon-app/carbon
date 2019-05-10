190fb87b4f04e132978851c6ed5c481ecb31ec49
Mike Fix <mrfix84@gmail.com>
2019-05-09 17:49:10 -0700

    clean up code readability

--- components/LineNumbersContainer.js
+++ components/LineNumbersContainer.js
@@ -4,36 +4,39 @@ function LineNumbersContainer(props) {
   const lastNumber = React.useRef(null)
 
   React.useEffect(() => {
-    const nodes = Array.from(document.querySelectorAll('.CodeMirror-linenumber'))
-      .slice(1)
-      .map(
-        (node, i) => {
-          function handler(e) {
-            const set = lastNumber.current
-            document.querySelectorAll('.CodeMirror-line').forEach((line, j, arr) => {
-              // TODO use state instead of mutation
-              if (i != j) {
-                if (set == null) {
-                  line.style.opacity = 0.5
+    const rows = Array.from(document.querySelectorAll('.CodeMirror-linenumber')).slice(1)
+    const lines = document.querySelectorAll('.CodeMirror-line')
+
+    const nodes = rows.map(
+      (row, i) => {
+        function handler(e) {
+          const prev = lastNumber.current
+
+          lines.forEach((line, j, arr) => {
+            if (i != j) {
+              if (prev == null) {
+                line.style.opacity = 0.5
+              }
+            } else {
+              if (e.shiftKey && prev != null) {
+                for (let index = Math.min(prev, i); index < Math.max(prev, i) + 1; index++) {
+                  arr[index].style.opacity = arr[prev].style.opacity
                 }
               } else {
-                if (e.shiftKey && set != null) {
-                  for (let index = Math.min(set, i); index < Math.max(set, i) + 1; index++) {
-                    arr[index].style.opacity = arr[set].style.opacity
-                  }
-                } else {
-                  line.style.opacity = line.style.opacity == 1 ? 0.5 : 1
-                }
+                line.style.opacity = line.style.opacity == 1 ? 0.5 : 1
               }
-            })
-            lastNumber.current = i
-          }
-          node.addEventListener('click', handler)
+            }
+          })
 
-          return [node, handler]
-        },
-        [props.on]
-      )
+          lastNumber.current = i
+        }
+
+        row.addEventListener('click', handler)
+
+        return [row, handler]
+      },
+      [props.on]
+    )
 
     return () => nodes.map(([n, fn]) => n.removeEventListener('click', fn))
   })
