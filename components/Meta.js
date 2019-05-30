7d65448428ad4ecb1477839ac1ce196aa2755aab
Mike Fix <mrfix84@gmail.com>
2019-05-29 17:56:24 -0700

    upgrade dependencies

--- components/Meta.js
+++ components/Meta.js
@@ -26,7 +26,7 @@ export const StylesheetLink = ({ theme }) => {
     href = `/static/themes/${theme}.css`
   } else {
     const themeDef = THEMES_HASH[theme]
-    href = `//cdnjs.cloudflare.com/ajax/libs/codemirror/5.42.2/theme/${themeDef &&
+    href = `//cdnjs.cloudflare.com/ajax/libs/codemirror/5.47.0/theme/${themeDef &&
       (themeDef.link || themeDef.id)}.min.css`
   }
 
@@ -34,7 +34,7 @@ export const StylesheetLink = ({ theme }) => {
 }
 
 export const CodeMirrorLink = () => (
-  <Link href="//cdnjs.cloudflare.com/ajax/libs/codemirror/5.42.2/codemirror.min.css" />
+  <Link href="//cdnjs.cloudflare.com/ajax/libs/codemirror/5.47.0/codemirror.min.css" />
 )
 
 const title = 'Carbon'
@@ -62,13 +62,13 @@ export const MetaTags = React.memo(() => (
 export const MetaLinks = React.memo(() => {
   return (
     <React.Fragment>
-      <Link href="//cdnjs.cloudflare.com/ajax/libs/codemirror/5.42.2/theme/seti.min.css" />
+      <Link href="//cdnjs.cloudflare.com/ajax/libs/codemirror/5.47.0/theme/seti.min.css" />
       <CodeMirrorLink />
       {LOCAL_STYLESHEETS.map(id => (
         <Link key={id} href={`/static/themes/${id}.css`} />
       ))}
       {CDN_STYLESHEETS.map(themeDef => {
-        const href = `//cdnjs.cloudflare.com/ajax/libs/codemirror/5.42.2/theme/${themeDef &&
+        const href = `//cdnjs.cloudflare.com/ajax/libs/codemirror/5.47.0/theme/${themeDef &&
           (themeDef.link || themeDef.id)}.min.css`
         return <Link key={themeDef.id} href={href} />
       })}
