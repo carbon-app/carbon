cc1bb19d488c045c1519954eb66d0d4869753a9a
Mike Fix <mrfix84@gmail.com>
2019-06-19 14:40:15 -0700

    move get and update into api layer

--- api/snippet.js
+++ api/snippet.js
@@ -1,51 +1,99 @@
-const micro = require('micro')
-const { json, createError } = require('micro')
+require('isomorphic-fetch')
 
+const url = require('url')
+const { json, createError, send, sendError } = require('micro')
+
+// ~ makes the file come later alphabetically, which is how gists are sorted
 const CARBON_STORAGE_KEY = '~carbon.json'
 
-async function updateSnippet(req) {
-  const id = req.url.split('/').pop()
-  if (id === 'snippet') {
-    // TODO use query param routing
-    return null
+function getSnippet(req) {
+  const parsed = url.parse(req.url, true)
+  const id = parsed.query.id
+
+  if (!id) {
+    throw createError(401, 'id is a required parameter')
   }
+
+  return fetch(`https://api.github.com/gists/${id}`, {
+    headers: {
+      Authorization: req.headers.Authorization || req.headers.authorization
+    }
+  })
+    .then(res => res.json())
+    .then(({ files, ...gist }) => {
+      let config
+      if (files[CARBON_STORAGE_KEY]) {
+        try {
+          config = JSON.parse(files[CARBON_STORAGE_KEY].content)
+        } catch (error) {
+          // pass
+        }
+      }
+
+      const otherFiles = Object.keys(files).filter(key => key !== CARBON_STORAGE_KEY)
+
+      const snippet = files[otherFiles[0]]
+
+      return {
+        gist: {
+          ...gist,
+          filename: otherFiles[0]
+        },
+        config: {
+          ...config,
+          code: snippet.content,
+          language: snippet.language && snippet.language.toLowerCase()
+        }
+      }
+    })
+}
+
+async function updateSnippet(req) {
+  const parsed = url.parse(req.url, true)
+  const id = parsed.query.id
+
   const { filename, code, ...config } = await json(req, { limit: '6mb' })
+
   // TODO filename's are required
   if (!id || !filename) {
-    throw createError(400, 'id and filename are required body parameters')
+    throw createError(401, 'id and filename are required body parameters')
   }
 
-  try {
-    const files = {
-      [filename]: {
-        content: code
-      }
+  const files = {
+    [filename]: {
+      content: code
     }
-
-    if (config) {
-      files[CARBON_STORAGE_KEY] = {
-        content: JSON.stringify(config)
-      }
-    }
-
-    return await fetch(`https://api.github.com/gists/${id}`, {
-      method: 'PATCH',
-      body: JSON.stringify({ files }),
-      headers: req.headers
-    }).then(res => res.json())
-  } catch (e) {
-    console.error(e)
-    return null
   }
+
+  if (config) {
+    files[CARBON_STORAGE_KEY] = {
+      content: JSON.stringify(config)
+    }
+  }
+
+  return fetch(`https://api.github.com/gists/${id}`, {
+    method: 'PATCH',
+    body: JSON.stringify({ files }),
+    headers: {
+      Authorization: req.headers.Authorization || req.headers.authorization
+    }
+  }).then(res => res.json())
 }
-module.exports = micro(function(req, res) {
-  switch (req.method) {
-    // case 'POST':
-    //   return createSnippet(req, res)
-    case 'PATCH':
-      return updateSnippet(req, res)
-    // default:
-    //   return getSnippet(req, res)
+
+module.exports = async function(req, res) {
+  try {
+    switch (req.method) {
+      // case 'POST':
+      //   return createSnippet(req, res)
+      case 'PATCH':
+        return send(res, 200, await updateSnippet(req, res))
+      case 'GET':
+        return send(res, 200, await getSnippet(req, res))
+    }
+  } catch (err) {
+    console.error(err)
+    send(res, err.statusCode || 500, err.message || err)
   }
-  throw new Error('Unimplemented')
-})
+
+  sendError(req, res, createError(501, 'Not Implemented'))
+}
