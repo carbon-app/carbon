af2155a7fcc8245d5397e55bfb4618e94deaae8d
Mike Fix <mrfix84@gmail.com>
2019-06-19 10:31:48 -0700

    fix service worker build - file exported to /public (unwanted)

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
-const path = require('path')
 const withOffline = require('next-offline')
 
 module.exports = withOffline({
   target: 'serverless',
   dontAutoRegisterSw: true,
   workboxOpts: {
-    swDest: path.join(__dirname, 'public/service-worker.js')
+    swDest: 'public/service-worker.js'
   },
   experimental: {
     autoExport: true
