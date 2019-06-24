fc25b439edf6c7540a40f7f5cd996592fb7cc577
Mike Fix <mrfix84@gmail.com>
2019-06-24 09:46:30 -0700

    try to fix SW

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = withOffline({
   target: 'serverless',
   dontAutoRegisterSw: true,
   workboxOpts: {
-    swDest: 'public/service-worker.js'
+    swDest: require('path').resolve(__dirname, 'public/service-worker.js')
   },
   experimental: {
     autoExport: true
