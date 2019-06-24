5b099b2f7b644c8e8bb02805aba6818739f94000
Mike Fix <mrfix84@gmail.com>
2019-06-24 15:36:55 -0700

    try a mix of configs

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
+const path = require('path')
 const withOffline = require('next-offline')
 
 module.exports = withOffline({
   target: 'serverless',
   dontAutoRegisterSw: true,
+  workboxOpts: {
+    swDest: path.join(__dirname, 'public/service-worker.js')
+  },
   experimental: {
     autoExport: true
   }
