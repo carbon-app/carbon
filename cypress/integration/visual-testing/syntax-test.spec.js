0765c47e0cfd440cb8c246424aa8c358655506fa
Mike Fix <mrfix84@gmail.com>
2019-08-08 17:48:18 -0700

    re-enabled visual regression testing

--- cypress/integration/visual-testing/syntax-test.spec.js
+++ cypress/integration/visual-testing/syntax-test.spec.js
@@ -1,6 +1,7 @@
-/* global cy,before,after */
+/* global cy, before, after */
 import { environment } from '../../util'
-describe.skip('Visual Regression Testing', () => {
+
+describe('Visual Regression Testing', () => {
   before(() => {
     cy.eyesOpen({
       appName: 'Carbon',
@@ -8,6 +9,7 @@ describe.skip('Visual Regression Testing', () => {
       browser: environment
     })
   })
+
   after(() => {
     cy.eyesClose()
   })
@@ -16,7 +18,7 @@ describe.skip('Visual Regression Testing', () => {
     cy.visit("/?code={name:'Andrew',age:30}&l=application%2Fjson")
     cy.eyesCheckWindow({
       tag: 'JSON',
-      sizeMode: 'selector', //mode
+      target: 'region',
       selector: '.page'
     })
   })
@@ -25,15 +27,16 @@ describe.skip('Visual Regression Testing', () => {
     cy.visit('/?code=class Program { static void Main(){ do }}&l=text%2Fx-csharp')
     cy.eyesCheckWindow({
       tag: 'C#',
-      sizeMode: 'selector', //mode
+      target: 'region',
       selector: '.page'
     })
   })
+
   it('syntax test for C++', () => {
     cy.visit('/?l=text%2Fx-c%2B%2Bsrc&code=for(size_t i=0 ;i<length; i%2B%2B){}')
     cy.eyesCheckWindow({
       tag: 'C++',
-      sizeMode: 'selector', //mode
+      target: 'region',
       selector: '.page'
     })
   })
