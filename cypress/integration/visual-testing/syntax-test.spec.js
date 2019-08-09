64dda5b9363c1b7c88cedc247c230d9957179317
Mike Fix <mrfix84@gmail.com>
2019-08-09 10:37:59 -0700

    clean up syntax-test

--- cypress/integration/visual-testing/syntax-test.spec.js
+++ cypress/integration/visual-testing/syntax-test.spec.js
@@ -14,30 +14,20 @@ describe('Visual Regression Testing', () => {
     cy.eyesClose()
   })
 
-  it('syntax test for JSON', () => {
-    cy.visit("/?code={name:'Andrew',age:30}&l=application%2Fjson")
-    cy.eyesCheckWindow({
-      tag: 'JSON',
-      target: 'region',
-      selector: '.page'
-    })
-  })
+  const cases = [
+    ['JSON', "/?code={name:'Andrew',age:30}&l=application%2Fjson"],
+    ['C#', '/?code=class Program { static void Main(){ do }}&l=text%2Fx-csharp'],
+    ['C++', '/?l=text%2Fx-c%2B%2Bsrc&code=for(size_t i=0 ;i<length; i%2B%2B){}']
+  ]
 
-  it('syntax test for C#', () => {
-    cy.visit('/?code=class Program { static void Main(){ do }}&l=text%2Fx-csharp')
-    cy.eyesCheckWindow({
-      tag: 'C#',
-      target: 'region',
-      selector: '.page'
-    })
-  })
-
-  it('syntax test for C++', () => {
-    cy.visit('/?l=text%2Fx-c%2B%2Bsrc&code=for(size_t i=0 ;i<length; i%2B%2B){}')
-    cy.eyesCheckWindow({
-      tag: 'C++',
-      target: 'region',
-      selector: '.page'
+  cases.forEach(([language, example]) => {
+    it(`Syntax test for "${language}"`, () => {
+      cy.visit(example)
+      cy.eyesCheckWindow({
+        tag: language,
+        target: 'region',
+        selector: '.page'
+      })
     })
   })
 })
