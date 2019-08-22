c0d63ad654dd03d02d24390adbfcab2bac5be30e
Mike Fix <mrfix84@gmail.com>
2019-08-22 16:46:20 -0700

    remove old clearbit

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,6 @@ export default class extends Document {
           <Main />
           <NextScript />
           <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
-          {/* <script src="https://reveal.clearbit.com/v1/companies/reveal?authorization=pk_f0bc52e62e19fe1a43cb47e95e090d89&variable=clearbit" /> */}
         </body>
       </html>
     )
