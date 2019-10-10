32eddfdfe44ab255cd0998bfbcbca1d66d609261
Ramy Majouji <ramy.majouji@glossier.com>
2019-10-10 16:57:48 -0500

    Clean up SVGs

--- components/svg/Controls.js
+++ components/svg/Controls.js
@@ -1,7 +1,10 @@
 import React from 'react'
 
 export const Controls = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="54" height="14" viewBox="0 0 54 14">
+  <svg xmlns="http://www.w3.org/2000/svg"
+    width="54"
+    height="14"
+    viewBox="0 0 54 14">
     <g fill="none" fillRule="evenodd" transform="translate(1 1)">
       <circle cx="6" cy="6" r="6" fill="#FF5F56" stroke="#E0443E" strokeWidth=".5" />
       <circle cx="26" cy="6" r="6" fill="#FFBD2E" stroke="#DEA123" strokeWidth=".5" />
@@ -12,7 +15,10 @@ export const Controls = () => (
 
 export const ControlsBW = () => (
   <svg xmlns="http://www.w3.org/2000/svg" width="54" height="14" viewBox="0 0 54 14">
-    <g fill="none" fillRule="evenodd" stroke="#878787" transform="translate(1 1)">
+    <g fill="none"
+      fillRule="evenodd"
+      stroke="#878787"
+      transform="translate(1 1)">
       <circle cx="6" cy="6" r="6" />
       <circle cx="26" cy="6" r="6" />
       <circle cx="46" cy="6" r="6" />
