30377741669b263be69c23f569b728566888a062
Mike Fix <mrfix84@gmail.com>
2020-05-14 21:50:29 -0700

    make next.config.js smaller

--- next.config.js
+++ next.config.js
@@ -1,24 +1,24 @@
 const bundleAnalyzer = require('@next/bundle-analyzer')
 const withOffline = require('next-offline')
 
-const withBundleAnalyzer = bundleAnalyzer({ enabled: true })
+const withBundleAnalyzer = bundleAnalyzer({ enabled: process.env.ANALYZE === 'true' })
 
-const config = withOffline({
-  target: 'serverless',
-  dontAutoRegisterSw: true,
-  // https://github.com/hanford/next-offline/blob/master/packages/now2-example/next.config.js
-  workboxOpts: {
-    swDest: 'static/service-worker.js',
-  },
-  webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.js$/,
-      include: /node_modules\/graphql-language-service-parser/,
-      use: [options.defaultLoaders.babel],
-    })
+module.exports = withBundleAnalyzer(
+  withOffline({
+    target: 'serverless',
+    dontAutoRegisterSw: true,
+    // https://github.com/hanford/next-offline/blob/master/packages/now2-example/next.config.js
+    workboxOpts: {
+      swDest: 'static/service-worker.js',
+    },
+    webpack: (config, options) => {
+      config.module.rules.push({
+        test: /\.js$/,
+        include: /node_modules\/graphql-language-service-parser/,
+        use: [options.defaultLoaders.babel],
+      })
 
-    return config
-  },
-})
-
-module.exports = process.env.ANALYZE === 'true' ? withBundleAnalyzer(config) : config
+      return config
+    },
+  })
+)
