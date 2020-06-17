9603fc513abe0da38c224988db2e5e3300815a2f
Peng Jie <bivinity.pengzjie@gmail.com>
2020-06-17 09:19:36 +0800

    refactor(color-picker): drag the pointer smoothly

--- components/ColorPicker.js
+++ components/ColorPicker.js
@@ -9,38 +9,57 @@ const pickerStyle = {
   margin: '0 auto 1px',
 }
 
-const ColorPicker = ({ onChange = () => {}, color, presets, style, disableAlpha }) => (
-  <React.Fragment>
-    <SketchPicker
-      styles={{ picker: style || pickerStyle }}
-      color={color}
-      onChangeComplete={onChange}
-      presetColors={presets}
-      disableAlpha={disableAlpha}
-    />
-    <style jsx>
-      {`
-        /* react-color overrides */
-        :global(.sketch-picker > div:nth-child(3) > div > div > span) {
-          color: ${COLORS.SECONDARY} !important;
-        }
+class ColorPicker extends React.PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = {
+      color: props.color,
+    }
+  }
 
-        :global(.sketch-picker > div:nth-child(3) > div > div > input) {
-          width: 100% !important;
-          box-shadow: none;
-          outline: none;
-          border-radius: 2px;
-          background: ${COLORS.DARK_GRAY};
-          color: #fff !important;
-        }
+  handleColorChange = color => {
+    this.setState({ color })
+  }
 
-        /* prettier-ignore */
-        :global(.sketch-picker > div:nth-child(2) > div:nth-child(1) > div:nth-child(2), .sketch-picker > div:nth-child(2) > div:nth-child(2)) {
-          background: #fff;
+  render() {
+    const { color } = this.state
+    const { onChange = () => {}, presets, style, disableAlpha } = this.props
+
+    return (
+      <React.Fragment>
+        <SketchPicker
+          styles={{ picker: style || pickerStyle }}
+          onChange={this.handleColorChange}
+          color={color}
+          onChangeComplete={onChange}
+          presetColors={presets}
+          disableAlpha={disableAlpha}
+        />
+        <style jsx>
+          {`
+            /* react-color overrides */
+            :global(.sketch-picker > div:nth-child(3) > div > div > span) {
+              color: ${COLORS.SECONDARY} !important
+            }
+
+            :global(.sketch-picker > div:nth-child(3) > div > div > input) {
+              width: 100% !important
+              box-shadow: none
+              outline: none
+              border-radius: 2px
+              background: ${COLORS.DARK_GRAY}
+              color: #fff !important
+            }
+
+            /* prettier-ignore */
+            :global(.sketch-picker > div:nth-child(2) > div:nth-child(1) > div:nth-child(2), .sketch-picker > div:nth-child(2) > div:nth-child(2)) {
+          background: #fff
         }
-      `}
-    </style>
-  </React.Fragment>
-)
+          `}
+        </style>
+      </React.Fragment>
+    )
+  }
+}
 
 export default ColorPicker
