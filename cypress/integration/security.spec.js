66d5f0c603e697b96412726b9988d27ea3d17f21
Mike Fix <mrfix84@gmail.com>
2020-07-29 18:42:29 -0700

    add redirect tests

--- cypress/integration/security.spec.js
+++ cypress/integration/security.spec.js
@@ -13,4 +13,16 @@ describe('security', () => {
 
     expect(stub).not.to.be.called
   })
+  it('/offsets -> Project Wren', () => {
+    cy.visit(`/offsets`)
+    cy.url().should('eq', 'https://projectwren.com/?utm_source=carbon')
+  })
+  it('/privacy -> Privacy policy', () => {
+    cy.visit(`/privacy`)
+    cy.url().should('eq', 'https://www.notion.so/PRIVACY-POLICY-e9847a7777714eb08ba15a7a8eaee937')
+  })
+  it('/terms -> Terms', () => {
+    cy.visit(`/terms`)
+    cy.url().should('eq', 'https://www.notion.so/TERMS-OF-USE-ff2ce22a7e9848c89c6be46b44297583')
+  })
 })
