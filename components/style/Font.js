34c25d54d4b2f77d8bec410d68ec90aa98df2614
rubjo <rune.bjorneras@gmail.com>
2020-08-07 06:49:49 +0000

    Added font: Victor Mono

--- components/style/Font.js
+++ components/style/Font.js
@@ -163,6 +163,26 @@ export default () => (
         unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC,
           U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
       }
-    `}
+
+      @font-face {
+        font-family: 'Victor Mono';
+        font-display: swap;
+        font-style: normal;
+        font-weight: 400;
+        src: local('Victor Mono'), local('VictorMono-Regular'),
+          url(https://unpkg.com/victormono/dist/woff2/VictorMono-Regular.woff2)
+            format('woff2');
+      }
+
+      @font-face {
+        font-family: 'Victor Mono';
+        font-display: swap;
+        font-style: italic;
+        font-weight: 400;
+        src: local('Victor Mono Italic'), local('VictorMono-Italic'),
+          url(https://unpkg.com/victormono/dist/woff2/VictorMono-Italic.woff2)
+            format('woff2');
+      }
+`}
   </style>
 )
