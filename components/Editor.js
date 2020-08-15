6139046f971037e7079477fe410c489dbb152122
Mike Fix <mrfix84@gmail.com>
2020-08-15 10:41:08 -0700

    comment out hacks

--- components/Editor.js
+++ components/Editor.js
@@ -125,17 +125,17 @@ class Editor extends React.Component {
 
     const node = this.carbonNode.current
 
-    const map = new Map()
-    if (isPNG) {
-      node.querySelectorAll('span[role="presentation"]').forEach(node => {
-        if (node.innerText && node.innerText.match(/%[A-Fa-f0-9]{2}/)) {
-          map.set(node, node.innerHTML)
-          node.innerText.match(/%[A-Fa-f0-9]{2}/g).forEach(t => {
-            node.innerHTML = node.innerHTML.replace(t, encodeURIComponent(t))
-          })
-        }
-      })
-    }
+    // const map = new Map()
+    // if (isPNG) {
+    //   node.querySelectorAll('span[role="presentation"]').forEach(node => {
+    //     if (node.innerText && node.innerText.match(/%[A-Fa-f0-9]{2}/)) {
+    //       map.set(node, node.innerHTML)
+    //       node.innerText.match(/%[A-Fa-f0-9]{2}/g).forEach(t => {
+    //         node.innerHTML = node.innerHTML.replace(t, encodeURIComponent(t))
+    //       })
+    //     }
+    //   })
+    // }
 
     const width = node.offsetWidth * exportSize
     const height = squared ? node.offsetWidth * exportSize : node.offsetHeight * exportSize
@@ -202,7 +202,7 @@ class Editor extends React.Component {
       // Twitter needs regular dataurls
       return await domtoimage.toPng(node, config)
     } finally {
-      map.forEach((value, node) => (node.innerHTML = value))
+      // map.forEach((value, node) => (node.innerHTML = value))
     }
   }
 
