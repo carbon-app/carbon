8397f1b48ab65dc408893837c9ec9d7cf0ea8402
Mike Fix <mrfix84@gmail.com>
2020-10-27 18:57:50 -0700

    add function names

--- components/WindowControls.js
+++ components/WindowControls.js
@@ -37,60 +37,63 @@ const CopyButton = React.memo(function CopyButton({ text }) {
 })
 
 const WINDOW_THEMES_MAP = { bw: <ControlsBW />, boxy: <ControlsBoxy /> }
-export default ({ theme, copyable, code, light }) => (
-  <div className="window-controls">
-    {WINDOW_THEMES_MAP[theme] || <Controls />}
-    <div className="window-title-container">
-      <input aria-label="Image Title" type="text" spellCheck="false" />
-    </div>
-    {copyable && (
-      <div className="copy-button">
-        <CopyButton text={code} />
+
+export default function WindowControls({ theme, copyable, code, light }) {
+  return (
+    <div className="window-controls">
+      {WINDOW_THEMES_MAP[theme] || <Controls />}
+      <div className="window-title-container">
+        <input aria-label="Image Title" type="text" spellCheck="false" />
       </div>
-    )}
-    <style jsx>
-      {`
-        .window-controls {
-          margin-top: -24px;
-          position: relative;
-          top: ${theme === 'bw' ? 36 : 34}px;
-          margin-left: ${theme === 'bw' ? 16 : 14}px;
-          margin-right: ${theme === 'boxy' ? 16 : 0}px;
-          z-index: 2;
-          text-align: ${theme === 'boxy' ? 'right' : 'initial'};
-        }
+      {copyable && (
+        <div className="copy-button">
+          <CopyButton text={code} />
+        </div>
+      )}
+      <style jsx>
+        {`
+          .window-controls {
+            margin-top: -24px;
+            position: relative;
+            top: ${theme === 'bw' ? 36 : 34}px;
+            margin-left: ${theme === 'bw' ? 16 : 14}px;
+            margin-right: ${theme === 'boxy' ? 16 : 0}px;
+            z-index: 2;
+            text-align: ${theme === 'boxy' ? 'right' : 'initial'};
+          }
 
-        .window-title-container {
-          position: absolute;
-          margin: 0px;
-          top: -3px;
-          left: -9px;
-          width: 100%;
-          text-align: center;
-        }
+          .window-title-container {
+            position: absolute;
+            margin: 0px;
+            top: -3px;
+            left: -9px;
+            width: 100%;
+            text-align: center;
+          }
 
-        input {
-          width: 250px;
-          background: none;
-          outline: none;
-          border: none;
-          text-align: center;
-          /**
-           * 140px is an arbitrary value, but it's roughly equal to:
-           * 2 * (window theme width + window theme outside margin)
-           */
-          max-width: calc(100% - 140px);
-          font-size: 14px;
-          color: ${light ? COLORS.BLACK : COLORS.SECONDARY};
-        }
+          input {
+            width: 250px;
+            background: none;
+            outline: none;
+            border: none;
+            text-align: center;
+            /**
+         * 140px is an arbitrary value, but it's roughly equal to:
+         * 2 * (window theme width + window theme outside margin)
+         */
+            max-width: calc(100% - 140px);
+            font-size: 14px;
+            color: ${light ? COLORS.BLACK : COLORS.SECONDARY};
+          }
 
-        .copy-button {
-          cursor: pointer;
-          position: absolute;
-          top: 0px;
-          right: 16px;
-        }
-      `}
-    </style>
-  </div>
-)
+          .copy-button {
+            cursor: pointer;
+            position: absolute;
+            top: 0px;
+            right: 16px;
+          }
+        `}
+      </style>
+    </div>
+  )
+}
--- components/svg/Remove.js
+++ components/svg/Remove.js
@@ -1,17 +1,19 @@
 import React from 'react'
 
-export default ({ color = 'black', style }) => (
-  <svg
-    width="5"
-    height="5"
-    viewBox="0 0 5 5"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    style={style}
-  >
-    <path
-      d="M2.5 3.08725L4.41704 5L5 4.41834L3.08857 2.5L5 0.581656L4.41704 0L2.5 1.91275L0.58296 0L0 0.581656L1.91144 2.5L0 4.41834L0.58296 5L2.5 3.08725Z"
-      fill={color}
-    />
-  </svg>
-)
+export default function Remove({ color = 'black', style }) {
+  return (
+    <svg
+      width="5"
+      height="5"
+      viewBox="0 0 5 5"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      style={style}
+    >
+      <path
+        d="M2.5 3.08725L4.41704 5L5 4.41834L3.08857 2.5L5 0.581656L4.41704 0L2.5 1.91275L0.58296 0L0 0.581656L1.91144 2.5L0 4.41834L0.58296 5L2.5 3.08725Z"
+        fill={color}
+      />
+    </svg>
+  )
+}
