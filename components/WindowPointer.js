eb5334bc6f6ea7605fc8807c9cf154f85642f703
Michael Fix <mrfix84@gmail.com>
2020-10-27 19:04:53 -0700

    Upgrade next (#1124)
    * upgrade to Next 10
    
    * add function names

--- components/WindowPointer.js
+++ components/WindowPointer.js
@@ -1,22 +1,24 @@
 import React from 'react'
 
-export default ({ fromLeft, fromRight, color = '#fff' }) => (
-  <div>
-    <div className="window-pointer" />
-    <style jsx>
-      {`
-        .window-pointer {
-          width: 0px;
-          height: 0px;
-          border-style: solid;
-          border-width: 0 5px 6px 5px;
-          border-color: transparent transparent ${color} transparent;
-          position: absolute;
-          top: -8px;
-          left: ${fromLeft || 'initial'};
-          right: ${fromRight || 'initial'};
-        }
-      `}
-    </style>
-  </div>
-)
+export default function WindowPointer({ fromLeft, fromRight, color = '#fff' }) {
+  return (
+    <div>
+      <div className="window-pointer" />
+      <style jsx>
+        {`
+          .window-pointer {
+            width: 0px;
+            height: 0px;
+            border-style: solid;
+            border-width: 0 5px 6px 5px;
+            border-color: transparent transparent ${color} transparent;
+            position: absolute;
+            top: -8px;
+            left: ${fromLeft || 'initial'};
+            right: ${fromRight || 'initial'};
+          }
+        `}
+      </style>
+    </div>
+  )
+}
