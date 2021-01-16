8196cf1e30a9df963fdd83de3b0490479e9a2151
Mike Fix <mrfix84@gmail.com>
2021-01-15 16:51:23 -0800

    combine tweet and imgur button components

--- components/ShareMenu.js
+++ components/ShareMenu.js
@@ -1,19 +1,44 @@
 import React from 'react'
+import { useAsyncCallback, useOnline as useOnlineListener } from 'actionsack'
 
+import { useAPI } from './ApiContext'
 import { COLORS } from '../lib/constants'
 import Button from './Button'
 import Popout, { managePopout } from './Popout'
-import TweetButton from './TweetButton'
-import ImgurButton from './ImgurButton'
 import { Down as ArrowDown } from './svg/Arrows'
 
 const popoutStyle = { width: '120px', right: 8 }
 
 function ShareMenu({ isVisible, toggleVisibility, tweet, imgur }) {
+  const api = useAPI()
+  const online = useOnlineListener()
+
+  const [onClickTweet, { loading: tweeting }] = useAsyncCallback(tweet)
+  const [uploadToImgur, { loading: imguring }] = useAsyncCallback(imgur)
+
+  if (!api || !api.tweet) {
+    return null
+  }
+
+  if (!online) {
+    return null
+  }
+
   return (
     <div className="share-menu-container">
       <div className="flex">
-        <TweetButton onClick={tweet} />
+        <Button
+          center
+          border
+          large
+          padding="0 16px"
+          margin="0"
+          onClick={onClickTweet}
+          color={COLORS.BLUE}
+          style={{ borderBottomRightRadius: 0, borderTopRightRadius: 0 }}
+        >
+          {tweeting ? 'Loading…' : 'Tweet'}
+        </Button>
         <Button
           id="export-menu"
           border
@@ -32,7 +57,9 @@ function ShareMenu({ isVisible, toggleVisibility, tweet, imgur }) {
       </div>
       <Popout hidden={!isVisible} borderColor={COLORS.BLUE} pointerRight="7px" style={popoutStyle}>
         <div className="share-row flex">
-          <ImgurButton onClick={imgur} />
+          <Button padding="8px" color={COLORS.BLUE} onClick={uploadToImgur}>
+            {imguring ? 'Uploading...' : 'Upload to Imgur'}
+          </Button>
         </div>
       </Popout>
       <style jsx>
@@ -46,11 +73,6 @@ function ShareMenu({ isVisible, toggleVisibility, tweet, imgur }) {
           .share-row {
             flex-direction: column;
             /* justify-content: space-between; */
-          }
-
-          .share-row > span {
-            padding: 8px;
-            margin: 0 auto;
           }
         `}
       </style>
