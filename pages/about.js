64cef8b37c41c60489196387deeee2032c46a231
Rizda Dwi Prasetya <rizda.dwi@gmail.com>
2021-10-06 11:48:49 +0000

    fix no longer valid gist based carbon url example

--- pages/about.js
+++ pages/about.js
@@ -76,9 +76,16 @@ export default function About() {
           <ul className="mt0 mb3">
             <li>Drop a file into the editor</li>
             <li>
-              Append a GitHub gist id to the url (
-              <a className="link" href="/0db00e81d5416c339181e59481c74b59">
-                example
+              Append a GitHub gist id to the url (e.g.{' '}
+              <a
+                className="link"
+                href="https://gist.github.com/mfix22/3208813b324d82a9ebd197e4b1c3bae8"
+              >
+                original gist url
+              </a>{' '}
+              ➔{' '}
+              <a className="link" href="/3208813b324d82a9ebd197e4b1c3bae8">
+                carbon url result
               </a>
               )
             </li>
