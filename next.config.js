0e7fc9f9160ac5c6c045eda3dc4bd4842c891f52
Michael Fix <mrfix84@gmail.com>
2021-10-16 16:10:48 -0700

    Try out next-pwa  (#1056)
    * try our next-pwa
    
    * remove unregister
    
    * skip waiting

--- next.config.js
+++ next.config.js
@@ -1,15 +1,16 @@
 const bundleAnalyzer = require('@next/bundle-analyzer')
-const withOffline = require('next-offline')
+const withOffline = require('next-pwa')
 
 const withBundleAnalyzer = bundleAnalyzer({ enabled: process.env.ANALYZE === 'true' })
 
 module.exports = withBundleAnalyzer(
   withOffline({
     target: 'serverless',
-    dontAutoRegisterSw: true,
-    // https://github.com/hanford/next-offline/blob/master/packages/now2-example/next.config.js
-    workboxOpts: {
-      swDest: 'static/service-worker.js',
+    pwa: {
+      disable: process.env.NODE_ENV !== 'production',
+      dest: 'public',
+      register: false,
+      skipWaiting: false,
     },
     webpack: (config, options) => {
       config.module.rules.push({
