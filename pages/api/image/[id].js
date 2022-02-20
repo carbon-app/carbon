9531a4fdd6fa6df5745c0463c9a0f87ebd246f3d
Mike Fix <mrfix84@gmail.com>
2022-02-19 16:56:27 -0800

    use local dom to image

--- pages/api/image/[id].js
+++ pages/api/image/[id].js
@@ -3,8 +3,7 @@ import qs from 'querystring'
 import chrome from 'chrome-aws-lambda'
 import puppeteer from 'puppeteer-core'
 
-// TODO expose local version of dom-to-image
-const DOM_TO_IMAGE_URL = 'https://unpkg.com/dom-to-image@2.6.0/dist/dom-to-image.min.js'
+const DOM_TO_IMAGE_PATH = require.resolve('../../../lib/dom-to-image')
 const NOTO_COLOR_EMOJI_URL =
   'https://raw.githack.com/googlei18n/noto-emoji/master/fonts/NotoColorEmoji.ttf'
 
@@ -59,7 +58,7 @@ export default async function id(req, res) {
     const queryString = state ? `state=${state}` : qs.stringify(params)
 
     await page.goto(`https://${host}/${id ? id : `?${queryString}`}`)
-    await page.addScriptTag({ url: DOM_TO_IMAGE_URL })
+    await page.addScriptTag({ path: DOM_TO_IMAGE_PATH })
 
     await page.waitForSelector('.export-container', { visible: true, timeout: 9500 })
 
